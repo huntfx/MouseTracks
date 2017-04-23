0695553985f26b6f2f57898d68166a848ad39808
Peter <Peter92@users.noreply.github.com>
2017-04-24 00:15:24 +0100

    Cleaned up code

--- _track_windows.py
+++ _track_windows.py
@@ -3,6 +3,7 @@ from _track_constants import MOUSE_BUTTONS
 import os
 
 def get_device_data():
+    """Get the resolution and refresh rate of the main monitor."""
     device = win32api.EnumDisplayDevices()
     settings = win32api.EnumDisplaySettings(device.DeviceName, 0)
     refresh_rate = getattr(settings, 'DisplayFrequency')
@@ -11,28 +12,25 @@ def get_device_data():
             'Refresh': refresh_rate, 1: refresh_rate}
 
 def get_cursor_pos():
+    """Return the cursor position as a tuple."""
     try:
         return win32api.GetCursorPos()
     except win32api.error:
         return None
 
-#GetAsyncKeyState won't work in full screen applications
-'''
 def get_mouse_click():
-    return any(win32api.GetAsyncKeyState(button) for button in MOUSE_BUTTONS)
-
-def get_key_press(key):
-    return win32api.GetAsyncKeyState(key)
-'''
-
-def get_mouse_click():
+    """Check if one of the three main mouse buttons is being clicked."""
     return any(win32api.GetKeyState(button) < 0 for button in MOUSE_BUTTONS)
 
-#Unfortunately this won't work either for the keyboard, dunno what will
 def get_key_press(key):
+    """Check if a key is being pressed.
+    Needs changing for something that detects keypresses in applications.
+    """
+    #return win32api.GetAsyncKeyState(key)
     return win32api.GetKeyState(key) < 0
 
 def remove_file(file_name):
+    """Delete a file."""
     try:
         os.remove(file_name)
     except WindowsError:
@@ -40,6 +38,7 @@ def remove_file(file_name):
     return True
 
 def rename_file(old_name, new_name):
+    """Rename a file."""
     try:
         os.rename(old_name, new_name)
     except WindowsError:
@@ -47,6 +46,7 @@ def rename_file(old_name, new_name):
     return True
 
 def create_folder(folder_path):
+    """Create a folder."""
     try:
         os.makedirs(folder_path)
     except WindowsError:
