f18d1ca465f3f25b796b2f4610fd9b0352e48757
Peter <Peter92@users.noreply.github.com>
2017-05-03 21:32:32 +0100

    Added a more information

--- core/versions.py
+++ core/versions.py
@@ -6,13 +6,20 @@ VERSION_HISTORY = [
 ]
 
 def upgrade_version(data):
-    """Files from an older version will be run through this function."""
+    """Files from an older version will be run through this function.
+
+    History:
+    2.0: Base script
+    2.0.1: Add acceleration tracking
+    """
     get_id = VERSION_HISTORY.index
     
-    current_version_id = VERSION_HISTORY.index(data['Version'])
-
+    current_version_id = get_id(data['Version'])
     if current_version_id < get_id('2.0.1'):
-        data['Acceleration'] = {}
+        try:
+            data['Acceleration']
+        except KeyError:
+            data['Acceleration'] = {}
 
     data['Version'] = VERSION
     return data
