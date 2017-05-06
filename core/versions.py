df4542cec14139a4706e949eadb8e918c4c00053
Peter <Peter92@users.noreply.github.com>
2017-05-06 05:48:28 +0100

    Automatically set version to latest

--- core/versions.py
+++ core/versions.py
@@ -1,9 +1,9 @@
-from constants import VERSION
-
 VERSION_HISTORY = [
     '2.0',
-    '2.0.1'
+    '2.0.1',
+    '2.0.1b'
 ]
+VERSION = VERSION_HISTORY[-1]
 
 def upgrade_version(data):
     """Files from an older version will be run through this function.
@@ -11,15 +11,19 @@ def upgrade_version(data):
     History:
     2.0: Base script
     2.0.1: Add acceleration tracking
+    2.0.1b: Rename acceleration to speed, change tracking method
     """
     get_id = VERSION_HISTORY.index
     
     current_version_id = get_id(str(data['Version']))
     if current_version_id < get_id('2.0.1'):
+        data['Acceleration'] = {}
+    if current_version_id < get_id('2.0.1b'):
         try:
-            data['Acceleration']
+            del data['Acceleration']
         except KeyError:
-            data['Acceleration'] = {}
-            
+            pass
+        data['Speed'] = {}
+    
     data['Version'] = VERSION
     return data
