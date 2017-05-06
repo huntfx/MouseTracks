48e072db60fc3df997f200e65cbe60cfeba5ee9f
Peter <Peter92@users.noreply.github.com>
2017-05-06 02:25:53 +0100

    Added function to calculate distance

--- core/functions.py
+++ core/functions.py
@@ -311,3 +311,9 @@ class SimpleConfig(object):
                     pass
         with open(self.file_name, 'w') as f:
             f.write('\n'.join(output))
+
+
+def find_distance(p1, p2):
+    """Find the distance between two (x, y) coordinates."""
+    return ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    
