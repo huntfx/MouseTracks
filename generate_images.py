0d68c3325b979f626481d5495f0997a8c7689243
Peter <Peter92@users.noreply.github.com>
2017-05-06 07:05:48 +0100

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -1,20 +1,8 @@
-from __future__ import division
-import numpy as np
+from core.image import RenderImage
 
-from core.constants import CONFIG
-from core.files import load_program
-from core.image import *
-from core.image_heatmap import generate as generate_heatmap
-from core.image_tracks import generate as generate_tracks
-
-#Options
 profile = 'default'
-desired_resolution = (CONFIG.data['GenerateImages']['OutputResolutionX'],
-                      CONFIG.data['GenerateImages']['OutputResolutionY'])
+r = RenderImage(profile)
 
-print 'Loading profile: {}'.format(profile)
-main_data = load_program(profile)
-print 'Desired resolution: {}x{}'.format(*desired_resolution)
-
-generate_tracks('Result/Recent Tracks.png', main_data['Tracks'])    
-generate_heatmap('Result/Recent Clicks.png', main_data['Clicks'])
+r.generate('Tracks')
+r.generate('Speed')
+r.generate('Clicks')
