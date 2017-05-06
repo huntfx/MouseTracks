5000141b14442d1fe93e9da2e95c791d9400f306
Peter <Peter92@users.noreply.github.com>
2017-05-06 07:04:25 +0100

    Merged image code into one file
    Also wrote a function to remove a lot of the duplicate lines

--- core/image.py
+++ core/image.py
@@ -1,9 +1,12 @@
 from __future__ import division
 import numpy as np
 from scipy.ndimage.interpolation import zoom
+from scipy.ndimage.filters import gaussian_filter
+from PIL import Image
 
 from constants import CONFIG, COLOURS_MAIN, COLOUR_MODIFIERS
-
+from functions import ColourRange
+from files import load_program
 
 def merge_array_max(arrays):
     if len(arrays) > 1:
@@ -31,7 +34,7 @@ def merge_resolutions(main_data, max_resolution=None, interpolate=True):
     highest_value = None
     lowest_value = None
     resolutions = main_data.keys()
-    
+
     if any(not isinstance(i, tuple) for i in resolutions) or any(len(i) != 2 for i in resolutions):
         raise ValueError('incorrect resolutions')
 
@@ -104,7 +107,8 @@ class ImageName(object):
     """Generate an image name using values defined in the config.
     Not implemented yet: creation time | modify time | exe name
     """
-    def __init__(self):
+    def __init__(self, program_name):
+        self.name = program_name
         self.reload()
 
     def reload(self):
@@ -119,8 +123,10 @@ class ImageName(object):
 
         self.track_colour = str(CONFIG.data['GenerateTracks']['ColourProfile'])
 
-    def generate(self, program_name, image_type):
-        if image_type.lower() == 'heatmap':
+        self.speed_colour = str(CONFIG.data['GenerateSpeedMap']['ColourProfile'])
+
+    def generate(self, image_type):
+        if image_type.lower() == 'clicks':
             name = CONFIG.data['GenerateHeatmap']['NameFormat']
             name = name.replace('[ExpMult]', self.heatmap_exp)
             name = name.replace('[GaussianSize]', self.heatmap_gaussian)
@@ -128,13 +134,17 @@ class ImageName(object):
         elif image_type.lower() == 'tracks':
             name = CONFIG.data['GenerateTracks']['NameFormat']
             name = name.replace('[ColourProfile]', self.track_colour)
+        elif image_type.lower() == 'speed':
+            name = CONFIG.data['GenerateSpeedMap']['NameFormat']
+            name = name.replace('[ColourProfile]', self.speed_colour)
         else:
-            raise ValueError('incorred image type, must be "tracks" or "heatmap"')
+            raise ValueError('incorred image type: {}, '
+                             'must be tracks, heatmap or speed'.format(image_type))
         name = name.replace('[UResX]', self.upscale_res_x)
         name = name.replace('[UResY]', self.upscale_res_y)
         name = name.replace('[ResX]', self.output_res_x)
         name = name.replace('[ResY]', self.output_res_y)
-        name = name.replace('[FriendlyName]', program_name)
+        name = name.replace('[FriendlyName]', self.name)
 
         #Replace invalid characters
         invalid_chars = ':*?"<>|'
@@ -223,6 +233,100 @@ def parse_colour_text(colour_name):
     return tuple(colours['Final'])
 
 
+def _click_heatmap(numpy_arrays):
+
+    #Add all arrays together
+    print 'Merging arrays...'
+    max_array = merge_array_add(numpy_arrays)
+
+    #Create a new numpy array and copy over the values
+    #I'm not sure if there's a way to skip this step since it seems a bit useless
+    print 'Converting to heatmap...'
+    h = len(max_array)
+    w = len(max_array[0])
+    heatmap = np.zeros(h * w).reshape((h, w))
+    height_range = range(h)
+    width_range = range(w)
+    exponential_multiplier = CONFIG.data['GenerateHeatmap']['ExponentialMultiplier']
+    for x in width_range:
+        for y in height_range:
+            heatmap[y][x] = max_array[y][x] ** exponential_multiplier
+
+    #Blur the array
+    print 'Applying gaussian blur...'
+    gaussian_blur = CONFIG.data['GenerateHeatmap']['GaussianBlurSize']
+    heatmap = gaussian_filter(heatmap, sigma=gaussian_blur)
+
+    #This part is temporary until the ColourRange function is fixed
+    heatmap *= 1000000
+
+    #Calculate the average of all the points
+    print 'Calculating average...'
+    total = [0, 0]
+    for x in width_range:
+        for y in height_range:
+            total[0] += 1
+            total[1] += heatmap[y][x]
+
+    #Set range of heatmap
+    min_value = 0
+    max_value = 10 * total[1] / total[0]
+    if CONFIG.data['GenerateHeatmap']['SetMaxRange']:
+        max_value = CONFIG.data['GenerateHeatmap']['SetMaxRange']
+        print 'Manually set highest range to {}'.format(max_value)
+    
+    #Convert each point to an RGB tuple
+    print 'Converting to RGB...'    
+    colour_map = CONFIG.data['GenerateHeatmap']['ColourProfile']
+    colour_range = ColourRange(max_value - min_value, ColourMap()[colour_map])
+    return Image.fromarray(convert_to_rgb(heatmap, colour_range))
+
+
+def arrays_to_colour(value_range, numpy_arrays, image_type):
+    image_type = image_type.lower()
+    if image_type not in ('tracks', 'speed'):
+        raise ValueError('image type must be given as either tracks or speed')
+    elif image_type == 'tracks':
+        colour_map = CONFIG.data['GenerateTracks']['ColourProfile']
+    elif image_type == 'speed':
+        colour_map = CONFIG.data['GenerateSpeedMap']['ColourProfile']
+
+    max_array = merge_array_max(numpy_arrays)
+    colour_range = ColourRange(value_range[1] - value_range[0], ColourMap()[colour_map])
+    return Image.fromarray(convert_to_rgb(max_array, colour_range))
+
+
+class RenderImage(object):
+    def __init__(self, profile):
+        self.profile = profile
+        self.data = load_program(profile)
+        self.name = ImageName(profile)
+
+    def generate(self, image_type):
+        image_type = image_type.lower()
+        if image_type not in ('tracks', 'speed', 'clicks'):
+            raise ValueError('image type must be given as either tracks, speed or clicks')
+        
+        if image_type == 'tracks':
+            value_range, numpy_arrays = merge_resolutions(self.data['Tracks'])
+            image_output = arrays_to_colour(value_range, numpy_arrays, 'Tracks')
+            image_name = self.name.generate('Tracks')
+        elif image_type == 'speed':
+            value_range, numpy_arrays = merge_resolutions(self.data['Speed'])
+            image_output = arrays_to_colour(value_range, numpy_arrays, 'Speed')
+            image_name = self.name.generate('Speed')
+        elif image_type == 'clicks':
+            value_range, numpy_arrays = merge_resolutions(self.data['Clicks'])
+            image_output = _click_heatmap(numpy_arrays)
+            image_name = self.name.generate('Clicks')
+        resolution = (CONFIG.data['GenerateImages']['OutputResolutionX'],
+                      CONFIG.data['GenerateImages']['OutputResolutionY'])
+        image_output = image_output.resize(resolution, Image.ANTIALIAS)
+        print 'Saving image...'
+        image_output.save(image_name)
+        print 'Finished saving.'
+
+
 class ColourMap(object):
     """Look up default colours or generate one if the set doesn't exist."""
     _MAPS = {
@@ -251,3 +355,4 @@ class ColourMap(object):
                 return generated_map
             else:
                 raise ValueError('unknown colour map')
+    
