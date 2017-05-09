bda5c00b3664044a4d882a4389d37ae2fccea1b5
Isometries <isometric_map@tutanota.com>
2017-05-09 03:59:32 -0400

    Updated
    Changed so that all the needed functions (except one) are in this single file. Also checks for all the dependencies here.

--- core/_os/linux/__init__.py
+++ core/_os/linux/__init__.py
@@ -1,11 +1,15 @@
 import os
 import subprocess
-import sys
+import re
 
 try:
     from Xlib import display
 except ImportError:
-    sys.exit()
+    raise NotImplemented('xlib not found')
+try:
+    import pyxhook
+except ImportError:
+    raise NotImplementedError('pyxhook not found')
 
 # removes a file, NOT directory
 def remove_file(file_name):
@@ -39,3 +43,17 @@ def get_running_processes():
         line_ = line.decode()
         pids += line_
     return pids
+
+# returns a string
+def get_refresh_rate():
+    raw_rate = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f9',shell=True, stdout=subprocess.PIPE).communicate()[0]
+    Hz = re.search('\d+\.\d+', raw_rate.decode()).group()
+    if Hz:
+        return Hz
+    else:
+        return '60'
+
+def get_resolution():
+    d = display.Display().screen()
+    return d.width_in_pixels, d.height_in_pixels
+
