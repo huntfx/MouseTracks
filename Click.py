c417c34c2245608084352e8d9ee70cffbf571a75
Peter <Peter92@users.noreply.github.com>
2017-05-10 03:29:41 +0100

    Link with get_mouse_click

--- Click.py
+++ Click.py
@@ -1,14 +1,27 @@
 from pyxhook import HookManager
 
-
-class mouse_click(HookManager):
-    def __init__(self, path):
+class MouseClick(HookManager):
+    def __init__(self):
         super().__init__()
-        self.points = []
-        new_hook = pyxhook.HookManager()
+        self.reset()
+        new_hook = HookManager()
         new_hook.MouseAllButtonsDown = self.click
         new_hook.start()
 
-    def click(self,event):
-        x, y = event.Position
-        self.points.append((x,y))
+    def click(self, event):
+        """Mark buttons as clicked."""
+        self.clicks[0] = 1
+    
+    def return_click(self):
+        """Get any pressed buttons and reset."""
+        clicks = self.clicks
+        self.reset()
+        return clicks
+    
+    def reset(self):
+        """Mark buttons as unclicked."""
+        self.clicks = [0]
+
+CLICKS = mouse_click()
+def get_mouse_click():
+    return CLICKS.return_click()
