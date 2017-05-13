2c5ca9fbe7b893e00661596cc7394336a23fe63e
Peter <Peter92@users.noreply.github.com>
2017-05-13 05:21:28 +0100

    Renamed to make it more obvious what to run

--- start_tracking.py
+++ start_tracking.py
@@ -4,9 +4,11 @@ from core.functions import error_output
 import traceback
     
 if __name__ == '__main__':
-    
-    CONFIG.save() # Rewrite the config with validated values
 
+    #Rewrite the config with validated values
+    CONFIG.save()
+
+    #Run the script and exit safely if an error happens
     try:
         error = start_tracking()
         if error.startswith('Traceback (most recent call last)'):
