dc26f0fc054c45438bc9cca0006f39b0880425d1
Peter <Peter92@users.noreply.github.com>
2017-05-15 01:41:58 +0100

    Use path given in config

--- core/files.py
+++ core/files.py
@@ -6,7 +6,14 @@ import os
 
 from _os import remove_file, rename_file, create_folder, hide_file
 from versions import VERSION, upgrade_version
-from constants import DEFAULT_NAME
+from constants import DEFAULT_NAME, CONFIG
+
+
+def format_folder_path(path):
+    parts = path.replace('\\', '/').split('/')
+    if '.' in parts[-1]:
+        del parts[-1]
+    return '/'.join(i for i in parts if i)
 
 
 def load_program(program_name=None):
@@ -15,13 +22,14 @@ def load_program(program_name=None):
     elif isinstance(program_name, (list, tuple)):
         program_name = program_name[0]
     name_format = re.sub('[^A-Za-z0-9]+', '', program_name).lower()
+    path = CONFIG.data['Paths']['Data']
     try:
-        with open('Data/{}.data'.format(name_format), 'rb') as f:
+        with open('{}/{}.data'.format(path, name_format), 'rb') as f:
             loaded_data = cPickle.loads(zlib.decompress(f.read()))
     except (IOError, zlib.error):
         try:
-            with open('Data/{}.data.old'.format(name_format), 'rb') as f:
-                loaded_data =  cPickle.loads(zlib.decompress(f.read()))
+            with open('{}/{}.data.old'.format(path, name_format), 'rb') as f:
+                loaded_data = cPickle.loads(zlib.decompress(f.read()))
         except (IOError, zlib.error):
             return {'Tracks': {},
                     'Clicks': {},
@@ -44,12 +52,13 @@ def save_program(program_name, data):
     data['LastSave'] = time.time()
     data['Version'] = VERSION
     compressed_data = zlib.compress(cPickle.dumps(data))
-        
-    old_name = 'Data/{}.data.old'.format(name_format)
-    new_name = 'Data/{}.data'.format(name_format)
-    temp_name = 'Data/{}.data.{}'.format(name_format, int(time.time()))
 
-    create_folder('Data')
+    path = CONFIG.data['Paths']['Data']
+    old_name = '{}/{}.data.old'.format(path, name_format)
+    new_name = '{}/{}.data'.format(path, name_format)
+    temp_name = '{}/{}.data.{}'.format(path, name_format, int(time.time()))
+
+    create_folder(path)
     with open(temp_name, 'wb') as f:
         f.write(compressed_data)
     remove_file(old_name)
