80be307c3d99cdad06abea17c6aecad8dba7c545
Peter <Peter92@users.noreply.github.com>
2017-05-16 15:59:26 +0100

    Made notify class easier to use

--- core/messages.py
+++ core/messages.py
@@ -14,8 +14,8 @@ MOUSE_UNCLICKED = 9
 MOUSE_CLICKED_OFFSCREEN = 10
 MOUSE_CLICKED_HELD = 11
 MOUSE_HELD = 12
-MOUSE_TRACK_COMPRESS_START = 13
-MOUSE_TRACK_COMPRESS_END = 14
+MOUSE_COMPRESS_START = 13
+MOUSE_COMPRESS_END = 14
 RESOLUTION_CHANGED = 16
 KEYBOARD_PRESSES = 32
 KEYBOARD_PRESSES_HELD = 33
@@ -36,13 +36,17 @@ DATA_LOADED = 82
 DATA_NOTFOUND = 83
 QUEUE_SIZE = 96
 
+
 def _mb_text(id):
     return ('Left', 'Middle', 'Right')[id]
 
+
 class Notify(object):
+    
     def __init__(self):
         self.reset()
-    def queue(self, message_id, *args):
+        
+    def __call__(self, message_id, *args):
         q0 = self.message_queue[0].append
         q1 = self.message_queue[1].append
         q2 = self.message_queue[2].append
@@ -70,10 +74,16 @@ class Notify(object):
             q0('Mouse button unclicked.')
         if message_id == MOUSE_HELD:
             q1('Mouse button being held.')
-        if message_id == MOUSE_TRACK_COMPRESS_START:
-            q2('Tracking data is being compressed...')
-        if message_id == MOUSE_TRACK_COMPRESS_END:
-            q2('Finished compressing.')
+        if message_id == MOUSE_COMPRESS_START:
+            if args[0] == 'track':
+                q2('Tracking data is being compressed...')
+            elif args[0] == 'speed':
+                q2('Speed data is being compressed...')
+        if message_id == MOUSE_COMPRESS_END:
+            if args[0] == 'track':
+                q2('Finished compressing tracking data.')
+            elif args[0] == 'speed':
+                q2('Finished compressing speed data.')
         if message_id == RESOLUTION_CHANGED:
             q2('Resolution changed from {}x{} to {}x{}'.format(args[0][0], args[0][1],
                                                                args[1][0], args[1][1]))
@@ -125,20 +135,16 @@ class Notify(object):
             q1('Started new data store.')
         if message_id == QUEUE_SIZE:
             q1('{} command{} queued for processing.'.format(args[0], '' if args[0] == 1 else 's'))
-            
-    def output(self):
+
+    def __str__(self):
         allowed_levels = range(MESSAGE_LEVEL, 3)
         output = [' | '.join(self.message_queue[i]) for i in allowed_levels][::-1]
         self.reset()
         message = ' | '.join(i for i in output if i)
         return message
-
+    
     def reset(self):
         self.message_queue = {0: [], 1: [], 2: []}
 
 
-def time_format(t):
-    return '[{}]'.format(datetime.fromtimestamp(t).strftime("%H:%M:%S"))
-
-
-notify = Notify()
+NOTIFY = Notify()
