6a86ab702652f379fcb208a0ecfc76ac6548628b
Peter <Peter92@users.noreply.github.com>
2017-05-17 23:22:14 +0100

    Moved xlib functions to new file

--- core/os/linux/__init__.py
+++ core/os/linux/__init__.py
@@ -1,27 +1,13 @@
 import os
 import subprocess
 import re
-try:
-    from Xlib import display
-except ImportError:
-    display = None
-try:
-    import pyxhook
-except ImportError:
-    pyxhook = None
-    
-from core.os.linux.click import get_mouse_click
 
-#Check modules exist or say which don't
-if display is None and pyxhook is None:
-    raise ImportError('pyxhook and xlib not found')
-elif display is None:
-    raise ImportError('xlib not found')
-elif pyxhook is None:
-    raise ImportError('pyxhook not found')
+try:
+    from core.os.linux.linux_xlib import *
+except ImportError:
+    raise ImportError('required modules not found')
 
-    
-# removes a file, NOT directory
+
 def remove_file(file_name):
     try:
         os.remove(file_name)
@@ -30,7 +16,6 @@ def remove_file(file_name):
     return True
 
 
-# will only replace files and empty directories
 def rename_file(old_name, new_name):
     try:
         os.replace(old_name, new_name)
@@ -53,29 +38,17 @@ def get_running_processes():
     for line in program_list.splitlines():
         line_ = line.decode()
         pids.append(line_)
-    output = {line.rsplit()[-1] : line.rsplit()[0] for line in pids}
+    output = {line.rsplit()[-1]: line.rsplit()[0] for line in pids}
     return output
 
 
-# returns a string
 def get_refresh_rate():
     raw_rate = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f9',shell=True, stdout=subprocess.PIPE).communicate()[0]
-    Hz = re.search('\d+\.\d+', raw_rate.decode()).group()
-    if Hz:
-        return Hz
+    refresh_rate = re.search('\d+\.\d+', raw_rate.decode()).group()
+    if refresh_rate:
+        return int(refresh_rate)
     else:
-        return '60'
-
-    
-def get_resolution():
-    d = display.Display().screen()
-    return d.width_in_pixels, d.height_in_pixels
-
-
-# should always return a tuple
-def get_cursor_pos():
-    d = display.Display().screen().root.query_pointer()
-    return d.root_x, d.root_y
+        return None
 
 
 #Placeholder functions
