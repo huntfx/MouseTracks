86dbc24be630eedc9054ee92426a42b772c8b34a
Peter <Peter92@users.noreply.github.com>
2017-05-17 13:49:44 +0100

    Update and rename core/_os/linux/__init__.py to core/os/linux/__init__.py

--- core/os/linux/__init__.py
+++ core/os/linux/__init__.py
@@ -1,16 +1,26 @@
 import os
 import subprocess
 import re
-
 try:
     from Xlib import display
 except ImportError:
-    raise NotImplemented('xlib not found')
+    display = None
 try:
     import pyxhook
 except ImportError:
-    raise NotImplementedError('pyxhook not found')
+    pyxhook = None
+    
+from core.os.linux.click import get_mouse_click
 
+#Check modules exist or say which don't
+if display is None and pyxhook is None:
+    raise ImportError('pyxhook and xlib not found')
+elif display is None:
+    raise ImportError('xlib not found')
+elif pyhook is None:
+    raise ImportError('pyxhook not found')
+
+    
 # removes a file, NOT directory
 def remove_file(file_name):
     try:
@@ -18,6 +28,7 @@ def remove_file(file_name):
     except FileNotFoundError:
         return False
     return True
+
 
 # will only replace files and empty directories
 def rename_file(old_name, new_name):
@@ -27,12 +38,14 @@ def rename_file(old_name, new_name):
         return False
     return True
 
+
 def create_folder(folder_path):
     try:
         os.makedirs(folder_path)
     except FileExistsError:
         return False
     return True
+
 
 def get_running_processes():
     pids = []
@@ -43,6 +56,7 @@ def get_running_processes():
     output = {line.rsplit()[-1] : line.rsplit()[0] for line in pids}
     return output
 
+
 # returns a string
 def get_refresh_rate():
     raw_rate = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f9',shell=True, stdout=subprocess.PIPE).communicate()[0]
@@ -52,11 +66,23 @@ def get_refresh_rate():
     else:
         return '60'
 
+    
 def get_resolution():
     d = display.Display().screen()
     return d.width_in_pixels, d.height_in_pixels
+
 
 # should always return a tuple
 def get_cursor_pos():
     d = display.Display().screen().root.query_pointer()
     return d.root_x, d.root_y
+
+
+#Placeholder functions
+KEYS = {}
+def get_key_press(key):
+    return False
+def show_file(file_name):
+    pass
+def hide_file(file_name):
+    pass
