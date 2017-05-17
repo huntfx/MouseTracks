1c9d32ff3383eef5e73cca6a9b878e57ea801d3c
Peter <Peter92@users.noreply.github.com>
2017-05-17 13:38:00 +0100

    Change default heatmap colours

--- core/constants.py
+++ core/constants.py
@@ -74,7 +74,7 @@ _config_defaults = [
         'MouseButtonRight': (True, bool),
         'GaussianBlurSize': (20, int),
         'ExponentialMultiplier': (1.0, float),
-        'ColourProfile': ('Heatmap', str),
+        'ColourProfile': ('Jet', str),
         'SetMaxRange': (0, int, 'Manually set the highest value.'
                                 ' Set to 0 to use auto, otherwise use trial and error'
                                 ' to get it right.')
