3b6dabfb7b86538b0fde97311923025194b9a85f
Peter <Peter92@users.noreply.github.com>
2017-05-20 17:33:57 +0100

    Cleaned up code to generate paths

--- core/files.py
+++ core/files.py
@@ -13,7 +13,7 @@ else:
     import pickle as cPickle
 
     
-DATA_FOLDER = CONFIG.data['Paths']['Data']
+DATA_FOLDER = CONFIG['Paths']['Data']
 DATA_NAME = '[PROGRAM].data'
 DATA_BACKUP_FOLDER = '.backup'
 DATA_TEMP_FOLDER = '.temp'
@@ -25,43 +25,14 @@ def format_folder_path(path):
         del parts[-1]
     return '/'.join(i for i in parts if i)
 
+    
+def _get_paths(program_name):
 
-def load_program(program_name=None):
     if program_name is None:
         program_name = DEFAULT_NAME
     elif isinstance(program_name, (list, tuple)):
         program_name = program_name[0]
     name_format = sub('[^A-Za-z0-9]+', '', program_name).lower()
-    path = CONFIG.data['Paths']['Data']
-    try:
-        with open('{}/{}.data'.format(path, name_format), 'rb') as f:
-            loaded_data = cPickle.loads(zlib.decompress(f.read()))
-    except (IOError, zlib.error):
-        try:
-            with open('{}/{}.data.old'.format(path, name_format), 'rb') as f:
-                loaded_data = cPickle.loads(zlib.decompress(f.read()))
-        except (IOError, zlib.error):
-            return {'Maps': {'Tracks': {}, 'Clicks': {}, 'Speed': {}, 'Combined': {},
-                             'Temp1': {}, 'Temp2': {}, 'Temp3': {}, 'Temp4': {},
-                             'Temp5': {}, 'Temp6': {}, 'Temp7': {}, 'Temp8': {}},
-                    'Keys': {'Pressed': {}, 'Held': {}},
-                    'Time': {'Created': time.time(),
-                             'Modified': time.time()},
-                    'Version': VERSION,
-                    'Ticks': {'Current': {'Tracks': 0, 'Speed': 0}, 'Total': 0, 'Recorded': 0},
-                    'TimesLoaded': 0}
-    else:
-        loaded_data['TimesLoaded'] += 1
-        return upgrade_version(loaded_data)
-
-
-def save_program(program_name, data):
-    if program_name is None:
-        program_name = [DEFAULT_NAME]
-    name_format = sub('[^A-Za-z0-9]+', '', program_name[0]).lower()
-    data['Time']['Modified'] = time.time()
-    data['Version'] = VERSION
-    compressed_data = zlib.compress(cPickle.dumps(data))
     
     name = DATA_NAME.replace('[PROGRAM]', name_format)
     new_name = '{}/{}'.format(DATA_FOLDER, name)
@@ -70,16 +41,60 @@ def save_program(program_name, data):
     temp_folder = '{}/{}'.format(DATA_FOLDER, DATA_TEMP_FOLDER)
     temp_name = '{}/{}'.format(temp_folder, name)
     
-    if create_folder(backup_folder):
-        hide_file(backup_folder)
-    if create_folder(temp_folder):
-        hide_file(temp_folder)
-    with open(temp_name, 'wb') as f:
+    return {'Main': new_name, 'Backup': backup_name, 'Temp': temp_name,
+            'BackupFolder': backup_folder, 'TempFolder': temp_folder}
+    
+
+def load_program(program_name=None):
+
+    paths = _get_paths(program_name)
+    
+    try:
+        #Load the main file
+        with open(paths['Main'], 'rb') as f:
+            loaded_data = cPickle.loads(zlib.decompress(f.read()))
+            
+    except (IOError, zlib.error):
+        #Load the backup file
+        try:
+            with open(paths['Backup'], 'rb') as f:
+                loaded_data = cPickle.loads(zlib.decompress(f.read()))
+                
+        #Create a new file
+        except (IOError, zlib.error):
+            return {'Maps': {'Tracks': {}, 'Clicks': {},
+                             'Temp1': {}, 'Temp2': {}, 'Temp3': {}, 'Temp4': {},
+                             'Temp5': {}, 'Temp6': {}, 'Temp7': {}, 'Temp8': {}},
+                    'Keys': {'Pressed': {}, 'Held': {}},
+                    'Time': {'Created': time.time(),
+                             'Modified': time.time()},
+                    'Version': VERSION,
+                    'Ticks': {'Current': {'Tracks': 0}, 'Total': 0, 'Recorded': 0},
+                    'TimesLoaded': 0}
+                    
+    else:
+        loaded_data['TimesLoaded'] += 1
+        return upgrade_version(loaded_data)
+
+
+def save_program(program_name, data):
+
+    data['Time']['Modified'] = time.time()
+    data['Version'] = VERSION
+    compressed_data = zlib.compress(cPickle.dumps(data))
+    
+    paths = _get_paths(program_name)
+    
+    if create_folder(paths['BackupFolder']):
+        hide_file(paths['BackupFolder'])
+    if create_folder(paths['TempFolder']):
+        hide_file(paths['TempFolder'])
+    with open(paths['Temp'], 'wb') as f:
         f.write(compressed_data)
-    remove_file(backup_name)
-    rename_file(new_name, backup_name)
-    if rename_file(temp_name, new_name):
+    remove_file(paths['Backup'])
+    rename_file(paths['Main'], paths['Backup'])
+    if rename_file(paths['Temp'], paths['Main']):
         return True
     else:
-        remove_file(temp_name)
+        remove_file(paths['Temp'])
         return False
