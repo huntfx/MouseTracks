40e7847c11042d0435528ef08d80e829d46b4d82
Peter <Peter92@users.noreply.github.com>
2017-06-21 14:17:54 +0100

    Option to generate all or most recent session

--- generate_images.py
+++ generate_images.py
@@ -1,4 +1,6 @@
 from __future__ import division
+from core.functions import ticks_to_seconds
+from core.constants import CONFIG
 
 
 def simple_bit_mask(selection, size, default_all=True):
@@ -37,19 +39,21 @@ def round_up(n):
 
 
 print('Type profile to load, or type "list" to see them all:')
-profile = raw_input()
+#profile = raw_input()
+profile = 'Default'
 
 if profile == 'list':
     
     from core.files import list_files, format_name
     from core.functions import RunningPrograms
+    from core.constants import DEFAULT_NAME
 
     #Read the data folder and format names
     all_files = sorted(list_files())
-    programs = {}
+    programs = {format_name(DEFAULT_NAME): DEFAULT_NAME}
     for program_name in RunningPrograms(list_only=True).programs.values():
         programs[format_name(program_name)] = program_name
-    all_files = [f for f in all_files if f in programs]
+    all_files = [f for f in all_files if f in programs or f == 'default']
 
     page = 1
     limit = 6
@@ -85,7 +89,7 @@ if profile == 'list':
         else:
             try:
                 num = int(profile) - 1
-                if not 0 < num <= maximum:
+                if not 0 <= num <= maximum:
                     raise IndexError
                 profile = programs[all_files[num]]
                 break
@@ -131,9 +135,30 @@ if generate_tracks or generate_heatmap:
     print('Loading profile {}...'.format(profile))
     r = RenderImage(profile)
 
+    last_session_start = r.data['Ticks']['Session']['Total']
+    last_session_end = r.data['Ticks']['Total']
+    ups = CONFIG['Main']['UpdatesPerSecond']
+    all_time = ticks_to_seconds(last_session_end, ups)
+    last_session_time = ticks_to_seconds(last_session_end - last_session_start, ups)
+    while True:
+        print('Would you like to generate everything or just the last session?')
+        print('1: Everything ({}) [Default]'.format(all_time))
+        print('2: Last Session ({})'.format(last_session_time))
+
+        result = simple_bit_mask(raw_input().split(), 2, default_all=False)
+        if result[0] and result[1]:
+            print('Please only select one option.')
+        elif result[1]:
+            last_session = True
+            break
+        else:
+            last_session = False
+            break
+
+    #Generate
     if generate_tracks:
-        r.generate('Tracks')
+        r.generate('Tracks', last_session)
     if generate_heatmap:
-        r.generate('Clicks')
+        r.generate('Clicks', last_session)
 else:
     print('Nothing was set to generate.')
