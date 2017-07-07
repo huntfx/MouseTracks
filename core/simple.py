3bbd02be2dfcf813711719eca14a4e081e5a7f3a
Peter <Peter92@users.noreply.github.com>
2017-07-07 23:38:15 +0100

    Improved file path formatting

--- core/simple.py
+++ core/simple.py
@@ -22,15 +22,33 @@ def round_up(n):
     
 
 def format_file_path(path):
+    """Process an input path and follow any environment variables."""
+    
+    #Set up any custom environment variables here
+    custom_paths = {
+        '%DOCUMENTS%': get_documents_path()
+    }
+    
+    repeat = False
     parts = path.replace('\\', '/').rstrip('/').split('/')
-    f = parts.pop(-1) if '.' in parts[-1] else None
+    file_name = parts.pop(-1) if '.' in parts[-1] else None
+    
+    #Process each part separately
     for i, part in enumerate(parts):
-        if part == '%DOCUMENTS%':
-            parts[i] = get_documents_path()
-        else:
+        try:
+            parts[i] = custom_paths[part]
+        except KeyError:
             env_var = read_env_var(part)
             if env_var is not None:
                 parts[i] = env_var
-    if f is not None:
-        parts.append(f)
-    return '/'.join(i.replace('\\', '/') for i in parts if i)
+        else:
+            if '%' in parts[i]:
+                repeat = True
+                
+    if file_name is not None:
+        parts.append(file_name)
+    final_path = '/'.join(i.replace('\\', '/') for i in parts if i)
+    
+    if repeat:
+        return format_file_path(final_path)
+    return final_path
