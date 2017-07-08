8ea4e55c9a9a2de6118a37950954804c18f62255
Peter <Peter92@users.noreply.github.com>
2017-07-08 05:45:45 +0100

    Catch any 404 errors

--- core/online.py
+++ core/online.py
@@ -1,7 +1,15 @@
-import urllib2
+from __future__ import absolute_import
 
+try:
+    import urllib2
+except ImportError:
+    import urllib.request as urllib2
+
+    
 def get_url_contents(url):
+    """Get data from a URL."""
+    
     try:
         return urllib2.urlopen(url)
-    except urllib2.URLError:
+    except (urllib2.URLError, urllib2.HTTPError):
         return None
