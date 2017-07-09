779989680dab3a13952c0f7b50634866ec5b960f
Peter <Peter92@users.noreply.github.com>
2017-07-10 00:06:59 +0100

    Reorganised a lot of functions

---
+++ core/misc.py
@@ -0,0 +1,66 @@
+from __future__ import division, absolute_import
+from threading import Thread
+from multiprocessing import Process, Queue
+import sys
+import os
+import time
+
+from core.basic import format_file_path
+from core.constants import CONFIG, DEFAULT_PATH
+from core.messages import print_override
+from core.notify import *
+from core.os import get_running_processes
+        
+    
+def error_output(trace):
+    """Any errors are sent to here."""
+    file_name = format_file_path('{}\\error.txt'.format(DEFAULT_PATH))
+    with open(file_name, 'w') as f:
+        f.write(trace)
+    print_override(trace)
+    time.sleep(5)
+
+
+class RefreshRateLimiter(object):
+    """Limit the loop to a fixed updates per second.
+    It works by detecting how long a frame should be,
+    and comparing it to how long it's already taken.
+    """
+    def __init__(self, ticks):
+        self.time = time.time()
+        self.frame_time = 1 / ticks
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        time_difference = time.time() - self.time
+        time.sleep(max(0, self.frame_time - time_difference))
+    
+
+def simple_bit_mask(selection, size, default_all=True):
+    """Turn a range of numbers into True and False.
+    For example, [1, 3, 4] would result in [True, False, True, True].
+    I'm aware it's probably a bit overkill, kinda liked the idea though.
+    """
+    
+    #Calculate total
+    total = 0
+    for n in selection:
+        try:
+            total += pow(2, int(n) - 1)
+        except ValueError:
+            pass
+    
+    #Convert to True or False
+    values = map(bool, list(map(int, str(bin(total))[2:]))[::-1])
+    size_difference = max(0, size - len(values))
+    if size_difference:
+        values += [False] * size_difference
+    
+    #Set to use everything if an empty selection is given
+    if default_all:
+        if not any(values):
+            values = [True] * size
+    
+    return values
