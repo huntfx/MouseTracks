4745f469e0a7e58c4dee72ec769cc181df7976c0
Peter <Peter92@users.noreply.github.com>
2017-07-10 00:07:59 +0100

    Check running applications

---
+++ core/applications.py
@@ -0,0 +1,190 @@
+from __future__ import absolute_import
+import time
+
+from core.constants import CONFIG
+from core.notify import *
+from core.files import format_file_path
+from core.os import get_running_processes
+from core.internet import get_url_contents
+
+
+APP_LIST_PATH = format_file_path(CONFIG['Paths']['AppList'])
+
+APP_LIST_URL = 'https://raw.githubusercontent.com/Peter92/MouseTrack/master/Program%20List.txt'
+
+_DEFAULT_TEXT = [
+    '// Type any apps you want to be tracked here.',
+    '// Two separate apps may have the same name, and will be tracked under the same file.',
+    '// Put each app on a new line, in the format "Game.exe: Name".'
+    ' The executable file is case sensitive.',
+    '// Alternatively if the app is already named with the correct name'
+    ', "Game.exe" by itself will use "Game" as its name.',
+    ''
+]
+
+
+def _format_app_text(app, friendly_name=None):
+
+    #Ignore comments
+    if not app or any(app.startswith(i) for i in ('#', ';', '/')):
+        return None
+        
+    #Detect different extensions
+    lowercase = app.lower()
+    if '.exe' in lowercase:
+        ext_len = len(lowercase.split('.exe')[0])
+        app_name = '{}.{}'.format(app[:ext_len], app[ext_len + 1:ext_len + 4])
+    elif '.app' in lowercase:
+        ext_len = len(lowercase.split('.app')[0])
+        app_name = '{}.{}'.format(app[:ext_len], app[ext_len + 1:ext_len + 4])
+    elif '.bin' in lowercase:
+        ext_len = len(lowercase.split('.bin')[0])
+        app_name = '{}.{}'.format(app[:ext_len], app[ext_len + 1:ext_len + 4])
+    else:
+        return None
+    
+    #Determine if name has been provided in file, or generate if not
+    if friendly_name is None:
+        try:
+            friendly_name = app[ext_len:].split(':', 1)[1].strip()
+        except IndexError:
+            friendly_name = app[:ext_len]
+        return (app_name, friendly_name)
+    
+    #Format line to save
+    else:
+        if app_name == friendly_name:
+            return app_name
+        else:
+            return '{}: {}'.format(app_name, friendly_name)
+            
+    return app
+
+    
+def _format_app_list(app_list):
+    """Take a list of text inputs and sort them into a dictionary."""
+
+    applications = {}
+    for app in app_list:
+        
+        app_info = _format_app_text(app.strip())
+        if app_info is not None:
+            app_name, friendly_name = app_info
+            applications[app_name] = friendly_name
+    
+    return applications
+
+
+def read_app_list(application_path=APP_LIST_PATH, allow_write=False):
+    """Read the application list file.
+    Optionally write a new one if it doesn't exist.
+    """
+    try:
+        with open(application_path, 'r') as f:
+            lines = f.readlines()
+    except IOError:
+        lines = _DEFAULT_TEXT + ['// game.exe: Name']
+        if allow_write:
+            with open(application_path, 'w') as f:
+                f.write('\r\n'.join(lines))
+
+    return _format_app_list(lines)
+
+    
+def download_app_list():
+    """Download from the online application list.
+    Requires the config to be set to allow internet access.
+    """
+    if not CONFIG['Internet']['Enable']:
+        return None
+    application_list_contents = get_url_contents(APP_LIST_URL)
+    if application_list_contents:
+        return _format_app_list(application_list_contents)
+    return None
+
+
+def update_app_list(applications, downloaded_applications=None):
+    """Combine the current application list with the downloaded one."""
+    if downloaded_applications is None:
+        downloaded_applications = download_app_list()
+    if downloaded_applications:
+        for k, v in downloaded_applications.iteritems():
+            if k not in applications:
+                applications[k] = v
+        return True
+    return False
+    
+
+class RunningApplications(object):
+    """Detect which programs are currently running."""
+
+    def __init__(self, application_path=APP_LIST_PATH, queue=None):
+        self.q = queue
+        self.refresh()
+        self.application_path = application_path
+        self.reload_file()
+
+    def reload_file(self):
+    
+        self.applications = read_app_list(self.application_path, allow_write=True)
+        
+        #Download from the internet and combine with the current list
+        last_updated = CONFIG['SavedSettings']['AppListUpdate']
+        update_frequency = CONFIG['Internet']['UpdateApplications']
+        
+        if not CONFIG['Internet']['Enable'] or not update_frequency:
+            return
+        
+        #Only update if requirements are met
+        next_update = time.time() - update_frequency
+        if not self.applications or not last_updated or last_updated < next_update:
+        
+            if self.q is not None:
+                NOTIFY(APPLIST_UPDATE_START)
+                NOTIFY.send(self.q)
+            
+            if update_app_list(self.applications):
+            
+                CONFIG['SavedSettings']['AppListUpdate'] = int(time.time())
+                CONFIG.save()
+                self.save_file()
+                
+                if self.q is not None:
+                    NOTIFY(APPLIST_UPDATE_END_SUCCESS)
+            else:
+                if self.q is not None:
+                    NOTIFY(APPLIST_UPDATE_END_FAIL)
+                    
+            NOTIFY.send(self.q)
+                
+    def refresh(self):
+        self.processes = get_running_processes()
+    
+    def save_file(self):
+        lines = _DEFAULT_TEXT
+        
+        sorted_program_list = sorted(self.applications.keys(), key=lambda s: s.lower())
+        
+        for program_info in sorted_program_list:
+            
+            friendly_name = self.applications[program_info]
+            
+            new_line = _format_app_text(program_info, friendly_name)
+            if new_line is not None:
+                lines.append(new_line)
+        
+        with open(self.application_path, 'w') as f:
+            f.write('\n'.join(lines))
+    
+    def check(self):
+        """Check for any programs in the list that are currently loaded.
+        Choose the one with the highest ID as it'll likely be the most recent one.
+        """
+        matching_programs = {}
+        for program in self.applications:
+            if program in self.processes:
+                matching_programs[self.processes[program]] = program
+        if not matching_programs:
+            return None
+        latest_program = matching_programs[max(matching_programs.keys())]
+        return (self.applications[latest_program], latest_program)
