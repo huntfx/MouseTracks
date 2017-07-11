652b1ed4de1b5aefdebce2ae694b0c3632cac932
Peter <Peter92@users.noreply.github.com>
2017-07-11 05:06:06 +0100

    Try open system language or fall on backup

--- core/language.py
+++ core/language.py
@@ -4,9 +4,12 @@ from locale import getdefaultlocale
 system_language = getdefaultlocale()[0]
 fallback_language = 'en_GB'
 
-def open_language_file(language):
-    with open('loc\\{}.txt'.format(language), 'r') as f:
-        data = f.read()
+def _open_language_file(language):
+    try:
+        with open('loc\\{}.txt'.format(language), 'r') as f:
+            data = f.read()
+    except ImportError:
+        return None
 
     variables = {}
     for line in data.split('\n'):
@@ -14,3 +17,12 @@ def open_language_file(language):
         variables[variable_name.strip()] = variable_string.strip()
     
     return variables
+
+    
+def open_language_file(fallback_language='en_GB'):
+    
+    language_text = _open_language_file(system_language)
+    if language_text is None:
+        language_text = _open_language_file(fallback_language)
+
+    return language_text
