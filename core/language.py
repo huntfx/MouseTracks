561bcd4521e3c7ba7b8cf1ec75e4b778d09a121c
Peter <Peter92@users.noreply.github.com>
2017-07-11 21:08:21 +0100

    Update names and read language from config

--- core/language.py
+++ core/language.py
@@ -1,10 +1,9 @@
-from locale import getdefaultlocale
+from __future__ import absolute_import
+
+from core.config import CONFIG
 
 
-system_language = getdefaultlocale()[0]
-fallback_language = 'en_GB'
-
-def _open_language_file(language):
+def open_language_file(language):
     try:
         with open('loc\\{}.txt'.format(language), 'r') as f:
             data = f.read()
@@ -13,16 +12,17 @@ def _open_language_file(language):
 
     variables = {}
     for line in data.split('\n'):
-        variable_name, variable_string = line.split('=', 1)
-        variables[variable_name.strip()] = variable_string.strip()
+        if '=' in line:
+            variable_name, variable_string = line.split('=', 1)
+            variables[variable_name.strip()] = variable_string.strip()
     
     return variables
 
     
-def open_language_file(fallback_language='en_GB'):
-    
-    language_text = _open_language_file(system_language)
+def get_language(fallback_language='en_GB'):
+    """Try load the config language, or fallback to English."""
+    language_text = open_language_file(CONFIG['Main']['Language'])
     if language_text is None:
-        language_text = _open_language_file(fallback_language)
+        language_text = open_language_file(fallback_language)
 
     return language_text
