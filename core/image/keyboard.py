1690c43b183e4f2e55e39dfcabd86de93b8c9998
Peter <Peter92@users.noreply.github.com>
2017-07-17 18:10:35 +0100

    Added display of how many times pressed
    Number fits up until 9999999 so shouldn't be a problem

--- core/image/keyboard.py
+++ core/image/keyboard.py
@@ -2,6 +2,13 @@ from PIL import Image, ImageFont, ImageDraw
 
 from core.colours import ColourRange, ColourMap
 
+KB_KEY_SIZE = 65
+KB_KEY_PADDING = 8
+KB_PADDING = 13
+KB_TEXT_SIZE_KEY = 17
+KB_TEXT_SIZE_AMOUNT = 12
+KB_TEXT_OFFSET = 5
+KB_TEXT_HEIGHT = 3
 
 class KeyboardButton(object):
     def __init__(self, x, y, x_len, y_len=None):
@@ -38,10 +45,7 @@ class KeyboardButton(object):
 class KeyboardGrid(object):
     FILL_COLOUR = (170, 170, 170)
     OUTLINE_COLOUR = (0, 0, 0)
-    def __init__(self, size, padding, edge, press_count={}):
-        self.size = size
-        self.padding = padding
-        self.edge = edge
+    def __init__(self, press_count={}):
         self.grid = []
         self.new_row()
         self.count = press_count
@@ -50,24 +54,28 @@ class KeyboardGrid(object):
         self.grid.append([])
         self.row = self.grid[-1]
         
-    def add_button(self, display_name, key_name, width=1, custom_colour=None):
-        pos_x = self.edge
-        _width = int(round(self.size * width))
-        _height = self.size
-        self.row.append([display_name, key_name, (_width, _height), custom_colour])
+    def add_button(self, name, width=1, custom_colour=None):
+        pos_x = KB_PADDING
+        _width = int(round(KB_KEY_SIZE * width))
+        _height = KB_KEY_SIZE
+        self.row.append([name, (_width, _height), custom_colour])
 
-    def generate_coordinates(self):
+    def generate_coordinates(self, key_names={}):
         image = {'Fill': {}, 'Outline': [], 'Text': []}
+        max_offset = {'X': 0, 'Y': 0}
 
         m = max(self.count.values())
-        c = ColourRange(0, m, ColourMap()['RedToGreen'])
+        c = ColourRange(0, m, ColourMap()['WhiteToGreen'])
         
-        y_offset = self.edge
+        y_offset = KB_PADDING
         for i, row in enumerate(self.grid):
-            x_offset = self.edge
-            for j, (display_name, key_name, (x, y), custom_colour) in enumerate(row):
+            x_offset = KB_PADDING
+            for j, (name, (x, y), custom_colour) in enumerate(row):
 
-                image['Text'].append(((x_offset, y_offset), display_name))
+                amount = self.count.get(name, 0)
+                display_name = key_names.get(name, name)
+                
+                image['Text'].append(((x_offset, y_offset), display_name, amount))
                 
                 button_coordinates = KeyboardButton(x_offset, y_offset, x, y)
                 image['Outline'] += button_coordinates.outline()
@@ -75,40 +83,77 @@ class KeyboardGrid(object):
                 if custom_colour is not None:
                     fill_colour = custom_colour
                 else:
-                    try:
-                        fill_colour = c[self.count[key_name]]
-                    except KeyError:
-                        fill_colour = c[0]
+                    fill_colour = c[amount]
                 try:
                     image['Fill'][fill_colour] += button_coordinates.fill()
                 except KeyError:
                     image['Fill'][fill_colour] = button_coordinates.fill()
                 
-                x_offset += self.padding + x
+                x_offset += KB_KEY_PADDING + x
 
             #Decrease size of empty row
             if row:
-                y_offset += self.size + self.padding
+                y_offset += KB_KEY_SIZE + KB_KEY_PADDING
             else:
-                y_offset += (self.size + self.padding) / 2
+                y_offset += (KB_KEY_SIZE + KB_KEY_PADDING) / 2
+                
+            max_offset['X'] = max(max_offset['X'], x_offset)
+            max_offset['Y'] = max(max_offset['Y'], y_offset)
 
-        return ((x_offset + self.edge, y_offset + self.edge), image)
+        return ((max_offset['X'] + KB_PADDING, max_offset['Y'] + KB_PADDING), image)
 
-keys={'A': 50, 'TAB': 2, 'Q': 35, 'S': 15}
+keys={'A': 50, 'TAB': 2, 'Q': 35, 'S': 15, 'L': 100}
+key_names = {
+    'CAPSLOCK': 'Caps Lock',
+    'COMMA': ',',
+    'FULLSTOP': '.',
+    'FORWARDSLASH': '/',
+    'BACKSLASH': '\\'
+}
 
-k = KeyboardGrid(57, 5, 13, press_count=keys)
-k.add_button('tab', 'TAB', 1.5)
-k.add_button('q', 'Q', 1)
-k.add_button('w', 'W', 1)
+k = KeyboardGrid(keys)
+k.add_button('Tab', 1.7)
+k.add_button('Q', 1)
+k.add_button('W', 1)
+k.add_button('E', 1)
+k.add_button('R', 1)
+k.add_button('T', 1)
+k.add_button('Y', 1)
+k.add_button('U', 1)
+k.add_button('I', 1)
+k.add_button('O', 1)
+k.add_button('P', 1)
+k.add_button('m', 1)
+k.add_button('m', 1)
 k.new_row()
+k.add_button('CAPSLOCK', 2.3)
+k.add_button('A', 1)
+k.add_button('S', 1)
+k.add_button('D', 1)
+k.add_button('F', 1)
+k.add_button('G', 1)
+k.add_button('H', 1)
+k.add_button('J', 1)
+k.add_button('K', 1)
+k.add_button('L', 1)
 k.new_row()
-k.add_button('caps lock', 'CAPSLOCK', 1.6)
-k.add_button('a', 'A', 1)
-k.add_button('s', 'S', 1)
-(width, height), coordinate_dict = k.generate_coordinates()
+k.add_button('LSHIFT', 1.5)
+k.add_button('BACKSLASH', 1)
+k.add_button('Z', 1)
+k.add_button('X', 1)
+k.add_button('C', 1)
+k.add_button('V', 1)
+k.add_button('B', 1)
+k.add_button('N', 1)
+k.add_button('M', 1)
+k.add_button('COMMA', 1)
+k.add_button('FULLSTOP', 1)
+k.add_button('FORWARDSLASH', 1)
+(width, height), coordinate_dict = k.generate_coordinates(key_names)
 
 #Create image object
 im = Image.new('RGB', (width, height))
+im.paste((255, 255, 255), (0, 0, width, height))
 px = im.load()
 
 for colour in coordinate_dict['Fill']:
@@ -116,12 +161,15 @@ for colour in coordinate_dict['Fill']:
         px[x, y] = colour
 for x, y in coordinate_dict['Outline']:
     px[x, y] = (0, 0, 0)
-
+    
 draw = ImageDraw.Draw(im)
-font = ImageFont.truetype('arial.ttf', size=12)
-for (x, y), text in coordinate_dict['Text']:
-    x += 5
-    y += 5
-    draw.text((x, y), text, font=font)
+font_key = ImageFont.truetype('arial.ttf', size=KB_TEXT_SIZE_KEY)
+font_amount = ImageFont.truetype('arial.ttf', size=KB_TEXT_SIZE_AMOUNT)
+for (x, y), text, amount in coordinate_dict['Text']:
+    x += KB_TEXT_OFFSET
+    y += KB_TEXT_OFFSET
+    draw.text((x, y), text, font=font_key, fill=(0, 0, 0))
+    y += KB_TEXT_SIZE_KEY + KB_TEXT_HEIGHT
+    draw.text((x, y), ' x{}'.format(amount), font=font_amount, fill=(0, 0, 0))
 
 im.save('testimage.png', 'PNG')
