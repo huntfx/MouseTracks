057853b87de91d5281c95da2491237f241fc06b2
Peter <Peter92@users.noreply.github.com>
2017-07-20 23:00:16 +0100

    Updated language import

--- core/notify.py
+++ core/notify.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import
 
 from core.config import CONFIG
 from core.constants import DEFAULT_NAME
-from core.language import get_language
+from core.language import Language
 
-_L = get_language()
+STRINGS = Language().get_strings()
 MESSAGE_DEBUG = -1
 MOUSE_UNDETECTED = 0
 MOUSE_DETECTED = 1
@@ -46,7 +46,7 @@ APPLIST_UPDATE_FAIL = 55
 
 
 def _mb_text(id):
-    return (_L['MOUSE_BUTTON_LEFT'], _L['MOUSE_BUTTON_MIDDLE'], _L['MOUSE_BUTTON_RIGHT'])[id]
+    return (STRINGS['MOUSE_BUTTON_LEFT'], STRINGS['MOUSE_BUTTON_MIDDLE'], STRINGS['MOUSE_BUTTON_RIGHT'])[id]
 
 
 class Notify(object):
@@ -62,49 +62,49 @@ class Notify(object):
         if message_id == MESSAGE_DEBUG:
             q2('Debug: {}'.format(args))
         if message_id == MOUSE_UNDETECTED:
-            q2(_L['MOUSE_NOT_DETECTED'])
+            q2(STRINGS['MOUSE_NOT_DETECTED'])
         if message_id == MOUSE_DETECTED:
-            q2(_L['MOUSE_DETECTED'])
+            q2(STRINGS['MOUSE_DETECTED'])
         if message_id == MOUSE_OFFSCREEN:
-            q1(_L['MOUSE_OFFSCREEN'])
+            q1(STRINGS['MOUSE_OFFSCREEN'])
         if message_id == MOUSE_ONSCREEN:
-            q1(_L['MOUSE_ONSCREEN'])
+            q1(STRINGS['MOUSE_ONSCREEN'])
         if message_id == MOUSE_POSITION:
-            q0(_L['MOUSE_POSITION'].format(X=args[0][0], Y=args[0][1]))
+            q0(STRINGS['MOUSE_POSITION'].format(X=args[0][0], Y=args[0][1]))
         if message_id == MOUSE_CLICKED:
-            q1(_L['MOUSE_CLICKED'].format(MB=_mb_text(args[1]),
+            q1(STRINGS['MOUSE_CLICKED'].format(MB=_mb_text(args[1]),
                                           X=args[0][0], Y=args[0][1]))
         if message_id == MOUSE_CLICKED_OFFSCREEN:
-            q1(_L['MOUSE_CLICKED_OFFSCREEN'].format(MB=_mb_text(args[1])))
+            q1(STRINGS['MOUSE_CLICKED_OFFSCREEN'].format(MB=_mb_text(args[1])))
         if message_id == MOUSE_CLICKED_HELD:
-            q1(_L['MOUSE_CLICKED_HELD'].format(MB=_mb_text(args[1]),
+            q1(STRINGS['MOUSE_CLICKED_HELD'].format(MB=_mb_text(args[1]),
                                                X=args[0][0], Y=args[0][1]))
         if message_id == MOUSE_UNCLICKED:
-            q0(_L['MOUSE_UNCLICKED'])
+            q0(STRINGS['MOUSE_UNCLICKED'])
         if message_id == MOUSE_HELD:
-            q1(_L['MOUSE_HELD'])
+            q1(STRINGS['MOUSE_HELD'])
         if message_id == TRACK_COMPRESS_START:
-            q2(_L['TRACK_COMPRESS_START'])
+            q2(STRINGS['TRACK_COMPRESS_START'])
         if message_id == TRACK_COMPRESS_END:
-            q2(_L['TRACK_COMPRESS_END'])
+            q2(STRINGS['TRACK_COMPRESS_END'])
         if message_id == RESOLUTION_CHANGED:
-            q2(_L['RESOLUTION_CHANGED'].format(X1=args[0][0], Y1=args[0][1],
+            q2(STRINGS['RESOLUTION_CHANGED'].format(X1=args[0][0], Y1=args[0][1],
                                                X2=args[1][0], Y2=args[1][1]))
         if message_id == MONITOR_CHANGED:
-            q1(_L['MONITOR_CHANGED'].format(X1=args[0][0], Y1=args[0][1],
+            q1(STRINGS['MONITOR_CHANGED'].format(X1=args[0][0], Y1=args[0][1],
                                             X2=args[1][0], Y2=args[1][1]))
         if message_id == KEYBOARD_PRESSES:
-            single = _L['PRESS_SINGLE']
-            plural = _L['PRESS_PLURAL']
-            q1(_L['KEYBOARD_PRESSES'].format(K=', '.join(*args),
+            single = STRINGS['PRESS_SINGLE']
+            plural = STRINGS['PRESS_PLURAL']
+            q1(STRINGS['KEYBOARD_PRESSES'].format(K=', '.join(*args),
                                              P=single if len(args[0]) == 1 else plural))
         if message_id == KEYBOARD_PRESSES_HELD:
-            single = _L['PRESS_SINGLE']
-            plural = _L['PRESS_PLURAL']
-            q1(_L['KEYBOARD_PRESSES_HELD'].format(K=', '.join(*args),
+            single = STRINGS['PRESS_SINGLE']
+            plural = STRINGS['PRESS_PLURAL']
+            q1(STRINGS['KEYBOARD_PRESSES_HELD'].format(K=', '.join(*args),
                                                   P=single if len(args[0]) == 1 else plural))
         if message_id == APPLICATION_STARTED:
-            q2(_L['APPLICATION_STARTED'].format(A=args[0][0]))
+            q2(STRINGS['APPLICATION_STARTED'].format(A=args[0][0]))
         if message_id == APPLICATION_LOADING:
             default = False
             try:
@@ -114,53 +114,53 @@ class Notify(object):
                 profile = DEFAULT_NAME
             else:
                 profile = args[0][0]
-            q2(_L['APPLICATION_LOADING'].format(A=profile))
+            q2(STRINGS['APPLICATION_LOADING'].format(A=profile))
         if message_id == APPLICATION_QUIT:
-            q2(_L['APPLICATION_QUIT'])
+            q2(STRINGS['APPLICATION_QUIT'])
         if message_id == APPLICATION_RELOAD:
-            q1(_L['APPLICATION_RELOAD'])
+            q1(STRINGS['APPLICATION_RELOAD'])
         if message_id == APPLICATION_LISTEN:
-            q1(_L['APPLICATION_LISTEN'])
+            q1(STRINGS['APPLICATION_LISTEN'])
         if message_id == APPLIST_UPDATE_START:
-            q1(_L['APPLIST_UPDATE_START'])
+            q1(STRINGS['APPLIST_UPDATE_START'])
         if message_id == APPLIST_UPDATE_SUCCESS:
-            q1(_L['APPLIST_UPDATE_SUCCESS'])
+            q1(STRINGS['APPLIST_UPDATE_SUCCESS'])
         if message_id == APPLIST_UPDATE_FAIL:
-            q1(_L['APPLIST_UPDATE_FAIL'])
+            q1(STRINGS['APPLIST_UPDATE_FAIL'])
         if message_id == SAVE_START:
-            q2(_L['SAVE_START'])
+            q2(STRINGS['SAVE_START'])
         if message_id == SAVE_SUCCESS:
-            q2(_L['SAVE_SUCCESS'])
+            q2(STRINGS['SAVE_SUCCESS'])
         if message_id == SAVE_FAIL:
-            q2(_L['SAVE_FAIL'])
+            q2(STRINGS['SAVE_FAIL'])
         if message_id == SAVE_FAIL_RETRY:
-            single = _L['SECOND_SINGLE']
-            plural = _L['SECOND_PLURAL']
-            q2(_L['SAVE_FAIL_RETRY'].format(T=args[0], S=single if args[0] == 1 else plural,
+            single = STRINGS['SECOND_SINGLE']
+            plural = STRINGS['SECOND_PLURAL']
+            q2(STRINGS['SAVE_FAIL_RETRY'].format(T=args[0], S=single if args[0] == 1 else plural,
                               C=args[1] + 1, M=args[2]))
         if message_id == SAVE_FAIL_END:
-                q2(_L['SAVE_FAIL_END'])
+                q2(STRINGS['SAVE_FAIL_END'])
         if message_id == SAVE_SKIP:
             if args[1] > 2:
-                q2(_L['SAVE_SKIP_NO_CHANGE'])
+                q2(STRINGS['SAVE_SKIP_NO_CHANGE'])
             else:
-                single = _L['SECOND_SINGLE']
-                plural = _L['SECOND_PLURAL']
-                q2(_L['SAVE_SKIP_INACTIVE'].format(T=args[0], S=single if args[0] == 1 else plural))
+                single = STRINGS['SECOND_SINGLE']
+                plural = STRINGS['SECOND_PLURAL']
+                q2(STRINGS['SAVE_SKIP_INACTIVE'].format(T=args[0], S=single if args[0] == 1 else plural))
         if message_id == SAVE_PREPARE:
-            q2(_L['SAVE_PREPARE'])
+            q2(STRINGS['SAVE_PREPARE'])
         if message_id == START_MAIN:
-            q2(_L['START_MAIN'])
+            q2(STRINGS['START_MAIN'])
         if message_id == START_THREAD:
-            q2(_L['START_THREAD'])
+            q2(STRINGS['START_THREAD'])
         if message_id == DATA_LOADED:
-            q1(_L['DATA_LOADED'])
+            q1(STRINGS['DATA_LOADED'])
         if message_id == DATA_NOTFOUND:
-            q1(_L['DATA_NOTFOUND'])
+            q1(STRINGS['DATA_NOTFOUND'])
         if message_id == QUEUE_SIZE:
-            single = _L['COMMAND_SINGLE']
-            plural = _L['COMMAND_PLURAL']
-            q1(_L['COMMAND_COUNT'].format(N=args[0], C=single if args[0] == 1 else plural))
+            single = STRINGS['COMMAND_SINGLE']
+            plural = STRINGS['COMMAND_PLURAL']
+            q1(STRINGS['COMMAND_COUNT'].format(N=args[0], C=single if args[0] == 1 else plural))
 
     def get_output(self):
         allowed_levels = range(CONFIG['Advanced']['MessageLevel'], 3)
