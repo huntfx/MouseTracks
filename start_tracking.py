700417dbe8b8080358b0ff05539ba3de1a4fa920
Peter <Peter92@users.noreply.github.com>
2017-08-15 23:03:21 +0100

    Cleaned up code

--- start_tracking.py
+++ start_tracking.py
@@ -1,23 +1,10 @@
-import time
-import traceback
+from __future__ import absolute_import
 from multiprocessing import freeze_support
 
-from core.config import CONFIG
-from core.misc import error_output
-from core.main import start_tracking
+from core.track import start_tracking
 
 
 if __name__ == '__main__':
-
+    
     freeze_support()
-
-    #Rewrite the config with validated values
-    CONFIG.save()
-
-    #Run the script and exit safely if an error happens
-    try:
-        error = start_tracking()
-        if error.startswith('Traceback (most recent call last)'):
-            error_output(error)
-    except Exception as e:
-        error_output(traceback.format_exc())
+    start_tracking()
