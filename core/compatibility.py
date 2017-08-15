ac263d39946c8fcff86f8edd8f0b5e305c14f499
Peter <Peter92@users.noreply.github.com>
2017-08-15 22:49:14 +0100

    More control over reading python version

--- core/compatibility.py
+++ core/compatibility.py
@@ -1,7 +1,5 @@
+from __future__ import absolute_import, print_function
 import sys
-
-
-PYTHON_VERSION = sys.version_info.major
 
 
 def get_items(d):
@@ -13,6 +11,71 @@ def get_items(d):
     else:
         return d.items()
 
+
+def _print(text):
+    """Send everything here to print, so that tweaks can be made if needed."""
+    for line in text.replace('\\n', '\n').split('\n'):
+        try:
+            print(line)
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            print(line.encode('utf-8').strip())
+
+
+class PythonVersion(object):
+    
+    MAJOR = sys.version_info.major
+    MINOR = sys.version_info.minor
+    MICRO = sys.version_info.micro
+    
+    def __init__(self):
+        pass
+    
+    def __float__(self):
+        return float('{}.{}'.format(self.MAJOR, self.MINOR))
+    
+    def __int__(self):
+        return int(self.MAJOR)
+    
+    def __str__(self):
+        return '{}.{}.{}'.format(str, (self.MAJOR, self.MINOR, self.MICRO))
+    
+    def _compare(self, value):
+        try:
+            v_num = int(value)
+        except ValueError:
+            try:
+                v_num = float(value)
+            except ValueError:
+                return str(self), value
+            return float(self), v_num
+        return int(self), v_num
+    
+    def __eq__(self, value):
+        v1, v2 = self._compare(value)
+        return v1 == v2
+    
+    def __ne__(self, value):
+        v1, v2 = self._compare(value)
+        return v1 != v2
+    
+    def __gt__(self, value):
+        v1, v2 = self._compare(value)
+        return v1 > v2
+    
+    def __ge__(self, value):
+        v1, v2 = self._compare(value)
+        return v1 >= v2
+    
+    def __lt__(self, value):
+        v1, v2 = self._compare(value)
+        return v1 < v2
+    
+    def __le__(self, value):
+        v1, v2 = self._compare(value)
+        return v1 <= v2
+
+
+PYTHON_VERSION = PythonVersion
 
 if PYTHON_VERSION < 3:
     input = raw_input
