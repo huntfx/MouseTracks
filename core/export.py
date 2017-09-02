bc38d1bc0141af4eae2fa0683de7ff4030f1da97
Peter <Peter92@users.noreply.github.com>
2017-09-02 16:53:29 +0100

    Link with config

--- core/export.py
+++ core/export.py
@@ -1,65 +1,82 @@
 from __future__ import absolute_import
 
-from core.compatibility import range, get_items
+from core.compatibility import range, get_items, _print
+from core.config import CONFIG
 from core.files import load_program
+from core.os import create_folder
 
 
 class ExportCSV(object):
-    def __init__(self, profile=None, data=None):
-        if profile is None:
-            if data is None:
-                raise TypeError('profile name or data needs to be given')
-        elif data is None:
+    def __init__(self, profile, data=None):
+        if data is None:
             data = load_program(profile, _update_version=False)
         self.profile = profile
         self.data = data
     
     def _generate(self, resolution, data):
-        output = [['0'] * resolution[0] for _ in range(resolution[1])]
         
+        if len(data) < CONFIG['GenerateCSV']['MinimumResPoints']:
+            return None
+        
+        #Build list
+        output = [['0'] * resolution[0] for _ in range(resolution[1])]
         for k, v in get_items(data):
             output[k[1]][k[0]] = str(v)
         
+        #Add axis
+        output_axis = [[''] + [str(i) for i in range(resolution[0])]]
+        for i, row in enumerate(output):
+            output_axis.append([str(i)] + row)
+        
         return '\n'.join(','.join(row) for row in output)
    
-    def tracks(self, file_name):
+    def tracks(self, image_name):
         
-        file_name = file_name.replace('[Name]', self.profile)
-        
+        _print('Generating CSV from tracks...')
         for resolution in self.data['Maps']['Tracks']:
+            CONFIG['GenerateImages']['_TempResolutionX'], CONFIG['GenerateImages']['_TempResolutionY'] = resolution
+            
             result = self._generate(resolution, self.data['Maps']['Tracks'][resolution])
             
-            res_name = file_name.replace('[Width]', str(resolution[0]))
-            res_name = res_name.replace('[Height]', str(resolution[1]))
-            with open(res_name, 'w') as f:
-                f.write(result)
+            if result is not None:
+                file_name = image_name.generate('csv-tracks', reload=True)
+                create_folder(file_name)
+                with open(file_name, 'w') as f:
+                    f.write(result)
    
-    def clicks(self, file_name):
-        
-        file_name = file_name.replace('[Name]', self.profile)
+    def clicks(self, image_name):
         
         mouse_buttons = ['LMB', 'MMB', 'RMB']
         
+        _print('Generating CSV from clicks...')
         for resolution in self.data['Maps']['Clicks']:
+            CONFIG['GenerateImages']['_TempResolutionX'], CONFIG['GenerateImages']['_TempResolutionY'] = resolution
+        
             for i, mb_clicks in enumerate(self.data['Maps']['Clicks'][resolution]):
                 if mb_clicks:
+                    
+                    CONFIG['GenerateHeatmap']['_MouseButtonLeft'] = i == 0
+                    CONFIG['GenerateHeatmap']['_MouseButtonMiddle'] = i == 1
+                    CONFIG['GenerateHeatmap']['_MouseButtonRight'] = i == 2
+        
                     result = self._generate(resolution, mb_clicks)
             
-                    res_name = file_name.replace('[Width]', str(resolution[0]))
-                    res_name = res_name.replace('[Height]', str(resolution[1]))
-                    res_name = res_name.replace('[MouseButton]', mouse_buttons[i])
-                    with open(res_name, 'w') as f:
-                        f.write(result)
+                    if result is not None:
+                        file_name = image_name.generate('csv-clicks', reload=True)
+                        create_folder(file_name)
+                        with open(file_name, 'w') as f:
+                            f.write(result)
     
-    def keys(self, file_name):
+    def keyboard(self, image_name):
         
-        file_name = file_name.replace('[Name]', self.profile)
-        
+        _print('Generating CSV from keyboard...')
         result = ['Key,Count,Time']
         for key in self.data['Keys']['All']['Pressed']:
             result.append('{},{},{}'.format(key, 
                                             self.data['Keys']['All']['Pressed'][key], 
                                             self.data['Keys']['All']['Held'][key]))
         
+        file_name = image_name.generate('csv-keyboard', reload=True)
+        create_folder(file_name)
         with open(file_name, 'w') as f:
             f.write('\n'.join(result))
