519ebf331531c8b5d4768173c5131b845035a79c
Peter <Peter92@users.noreply.github.com>
2017-09-02 16:54:35 +0100

    Added CSV and key presses per hour

--- core/image/__init__.py
+++ core/image/__init__.py
@@ -5,9 +5,10 @@ from core.image.keyboard import DrawKeyboard
 from core.image.calculate import merge_resolutions, convert_to_rgb, arrays_to_heatmap, arrays_to_colour, calculate_gaussian_size
 from core.image.colours import ColourRange, calculate_colour_map
 from core.constants import UPDATES_PER_SECOND
-from core.compatibility import get_items, _print, get_items
+from core.compatibility import get_items, _print
 from core.config import CONFIG, _config_defaults
 from core.constants import format_file_path
+from core.export import ExportCSV
 from core.files import load_program, format_name
 from core.maths import round_int
 from core.os import create_folder
@@ -30,7 +31,7 @@ class ImageName(object):
         'UpscaleHeight': ['UpscaleY', 'UpscaleResolutionY', 'UpscaleHeight', 'UHeight', 'UY'],
         'Exponential': ['ExponentialMultiplier', 'ExpMult'],
         'Colours': ['Colors', 'ColourMap', 'ColorMap', 'ColourProfile', 'ColorProfile'],
-        'MouseButtons': [],
+        'MouseButton': ['MouseButtons'],
         'FileName': ['FName'],
         'FirstSave': ['CTime', 'CreationTime', 'Created'], #1494809091
         'LatestSave': ['LastSave', 'MTime', 'ModifiedTime', 'LastModified'], #1503929321
@@ -68,8 +69,10 @@ class ImageName(object):
         g_t = CONFIG['GenerateTracks']
         g_kb = CONFIG['GenerateKeyboard']
     
-        self.width = str(g_im['OutputResolutionX'])
-        self.height = str(g_im['OutputResolutionY'])
+        #self.width = str(g_im['OutputResolutionX'])
+        #self.height = str(g_im['OutputResolutionY'])
+        self.width = str(g_im['_TempResolutionX'])
+        self.height = str(g_im['_TempResolutionY'])
         self.uwidth = str(g_im['_UpscaleResolutionX'])
         self.uheight = str(g_im['_UpscaleResolutionY'])
         self.high_precision = 'High Detail' if g_im['HighPrecision'] else 'Normal'
@@ -111,11 +114,17 @@ class ImageName(object):
         
         lookup = {'clicks': 'GenerateHeatmap',
                   'tracks': 'GenerateTracks',
-                  'keyboard': 'GenerateKeyboard'}
+                  'keyboard': 'GenerateKeyboard',
+                  'csv-tracks': 'NameFormatTracks',
+                  'csv-clicks': 'NameFormatClicks',
+                  'csv-keyboard': 'NameFormatKeyboard'}
         try:
             name = CONFIG[lookup[image_type]]['NameFormat']
         except KeyError:
-            raise ValueError('incorred image type: {}'.format(image_type))
+            try:
+                name = CONFIG['GenerateCSV'][lookup[image_type]]
+            except KeyError:
+                raise ValueError('incorred image type: {}'.format(image_type))
         
         #Rename alternative variables
         for k, v in get_items(self.ALTERNATIVES):
@@ -151,7 +160,7 @@ class ImageName(object):
         if image_type == 'clicks':
             name = name.replace('[Exponential]', self.heatmap_exponential)
             name = name.replace('[Colours]', self.heatmap_colours)
-            name = name.replace('[MouseButtons]', self.heatmap_button_group)
+            name = name.replace('[MouseButton]', self.heatmap_button_group)
             name = name.replace('[GaussianBlur]', self.heatmap_gaussian)
             name = name.replace('[GaussianSigma]', self.heatmap_gaussian_actual)
             name = name.replace('[RangeLimit]', self.heatmap_max)
@@ -166,6 +175,13 @@ class ImageName(object):
             name = name.replace('[Mapping]', self.keyboard_mapping)
             name = name.replace('[Size]', self.keyboard_size_mult)
             name = name.replace('[Extended]', self.keyboard_extended)
+        
+        elif image_type.startswith('csv'):
+            if image_type == 'csv-clicks':
+                
+                #Using the heatmap mouse buttons saves rewriting parts of the function,
+                #but the config will need edited first to only have one button selected.
+                name = name.replace('[MouseButton]', self.heatmap_button_group)
             
         else:
             raise ValueError('incorred image type: {}'.format(image_type))
@@ -175,8 +191,13 @@ class ImageName(object):
         for char in invalid_chars:
             if char in name:
                 name = name.replace(char, '')
+                
+        if image_type.startswith('csv'):
+            ext = 'csv'
+        else:
+            ext = CONFIG['GenerateImages']['FileType']
         
-        return '{}.{}'.format(format_file_path(name), CONFIG['GenerateImages']['FileType'])
+        return '{}.{}'.format(format_file_path(name), ext)
 
 
 class RenderImage(object):
@@ -188,22 +209,61 @@ class RenderImage(object):
             self.data = data
         self.name = ImageName(profile, data=self.data)
 
+    def keys_per_hour(self, session=False):
+        """Detect if the game has keyboard tracking or not.
+        Based on my own tracks, a game may range from 100 to 4000 normally.
+        Without keyboard tracking, it's generally between 0.01 and 5.
+        Check if this number is above 10 or 20 to get an idea.
+        """
+        
+        if session:
+            all_clicks = self.data['Keys']['Session']['Held']
+            ticks = self.data['Ticks']['Session']['Total']
+        else:
+            all_clicks = self.data['Keys']['All']['Held']
+            ticks = self.data['Ticks']['Total']
+            
+        include = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '
+        total_presses = sum(v for k, v in get_items(all_clicks) if k in include)
+        return 3600 * total_presses / ticks
+        
+    def csv(self):
+        
+        export = ExportCSV(self.profile, self.data)
+        
+        if CONFIG['GenerateCSV']['_GenerateTracks']:
+            csv_name = self.name.generate('csv-tracks', reload=True)
+            export.tracks(self.name)
+            
+        if CONFIG['GenerateCSV']['_GenerateClicks']:
+            export.clicks(self.name)
+            
+        if CONFIG['GenerateCSV']['_GenerateKeyboard']:
+            csv_name = self.name.generate('csv-keyboard', reload=True)
+            export.keyboard(self.name)
+            
+        
     def generate(self, image_type, last_session=False, save_image=True):
         image_type = image_type.lower()
         if image_type not in ('tracks', 'clicks', 'keyboard'):
             raise ValueError('image type \'{}\' not supported'.format(image_type))
         
-        session_start = self.data['Ticks']['Session']['Current'] if last_session else None
+        
         if not self.data['Ticks']['Total']:
             image_output = None
         
         else:
+        
+            CONFIG['GenerateImages']['_TempResolutionX'] = CONFIG['GenerateImages']['OutputResolutionX']
+            CONFIG['GenerateImages']['_TempResolutionY'] = CONFIG['GenerateImages']['OutputResolutionY']
             
             high_precision = CONFIG['GenerateImages']['HighPrecision']
             allow_resize = True
             
             #Generate mouse tracks image
             if image_type == 'tracks':
+            
+                session_start = self.data['Ticks']['Session']['Tracks'] if last_session else None
                 (min_value, max_value), numpy_arrays = merge_resolutions(self.data['Maps']['Tracks'], 
                                                                          session_start=session_start,
                                                                          high_precision=high_precision)
@@ -222,10 +282,12 @@ class RenderImage(object):
                 mmb = CONFIG['GenerateHeatmap']['_MouseButtonMiddle']
                 rmb = CONFIG['GenerateHeatmap']['_MouseButtonRight']
                 mb = [i for i, v in enumerate((lmb, mmb, rmb)) if v]
-                clicks = self.data['Maps']['Clicks']
+                if last_session:
+                    clicks = self.data['Maps']['Session']['Clicks']
+                else:
+                    clicks = self.data['Maps']['Clicks']
                 numpy_arrays = merge_resolutions(self.data['Maps']['Clicks'], multiple_selection=mb, 
-                                                 session_start=session_start, _find_range=False,
-                                                 high_precision=False)
+                                                 _find_range=False, high_precision=False)
                 
                 _h, _w = numpy_arrays[0].shape
                 gaussian_size = calculate_gaussian_size(_w, _h)
