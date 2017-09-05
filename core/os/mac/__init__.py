eb8ccf1f6042f6fb0dd4e891fd2f47d1ba351f92
Peter <Peter92@users.noreply.github.com>
2017-09-05 14:51:27 +0100

    Removed placeholders

--- core/os/mac/__init__.py
+++ core/os/mac/__init__.py
@@ -1,15 +1,12 @@
+from __future__ import absolute_import
 import subprocess
 
 try:
     from core.os.mac._appkit import *
 except ImportError:
-    raise ImportError('required modules not found')
-try:
-    from core.os.mac.placeholders import *
-except ImportError:
     pass
- 
- 
+    
+
 def get_running_processes():
     pids = []
     program_list = subprocess.Popen('ps -d', shell=True, stdout=subprocess.PIPE).communicate()[0]
