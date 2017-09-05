b77925499225bbf0aca1952a9f97bbeedd09202b
Peter <Peter92@users.noreply.github.com>
2017-09-05 14:57:28 +0100

    Remove placeholder functions

--- core/os/linux/__init__.py
+++ core/os/linux/__init__.py
@@ -1,19 +1,17 @@
-import os
 import subprocess
 import re
 
 try:
-    from core.os.linux.linux_xlib import *
+    from core.os.linux._xlib import *
 except ImportError:
-    raise ImportError('required modules not found')
+    pass
 
 
 def get_running_processes():
     pids = []
     program_list = subprocess.Popen('ps -d', shell=True, stdout=subprocess.PIPE).communicate()[0]
     for line in program_list.splitlines():
-        line_ = line.decode()
-        pids.append(line_)
+        pids.append(line.decode())
     output = {line.rsplit()[-1]: line.rsplit()[0] for line in pids}
     return output
 
@@ -25,13 +23,3 @@ def get_refresh_rate():
         return int(refresh_rate)
     else:
         return None
-
-
-#Placeholder functions
-KEYS = {}
-def get_key_press(key):
-    return False
-def show_file(file_name):
-    pass
-def hide_file(file_name):
-    pass
