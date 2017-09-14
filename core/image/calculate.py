22eff6cf698d6f76994098e20d3bc1711e490f72
Peter <Peter92@users.noreply.github.com>
2017-09-14 17:37:46 +0100

    Works with new file format

--- core/image/calculate.py
+++ core/image/calculate.py
@@ -2,15 +2,14 @@ from __future__ import division, absolute_import
 from multiprocessing import Process, Queue, cpu_count
 from PIL import Image
 
-#Attempt to import scipy, but have fallback options as scipy results in a huge 300mb+ exe file
 from core.image.scipy import blur, upscale
-from core.image.numpy import *
 from core.compatibility import range, _print
 from core.config import CONFIG
 from core.maths import round_int
+import core.numpy as numpy
 
 
-def calculate_gaussian_size(width, height):
+def gaussian_size(width, height):
     """Calculate correct size of gaussian blur.
     Currently only height is taken into account, but this could change, so takes both values.
     """
@@ -22,20 +21,18 @@ def calculate_gaussian_size(width, height):
         raise ValueError('invalid input type, must be int')
 
 
-def merge_resolutions(main_data, multiple_selection=False, 
-                      session_start=None, high_precision=False, _find_range=True):
+def merge_resolutions(main_data, map_selection=False, 
+                      session_start=None, high_precision=False):
     """Upscale each resolution to make them all match.
     A list of arrays and range of data will be returned.
     """
     
     numpy_arrays = []
-    highest_value = None
-    lowest_value = None
-    resolutions = main_data.keys()
-
-    if any(not isinstance(i, tuple) for i in resolutions) or any(len(i) != 2 for i in resolutions):
-        raise ValueError('incorrect resolutions')
-
+    if map_selection:
+        resolutions = main_data[map_selection[0]].keys()
+    else:
+        resolutions = main_data.keys()
+    print resolutions
             
     output_resolution = (CONFIG['GenerateImages']['OutputResolutionX'],
                          CONFIG['GenerateImages']['OutputResolutionY'])
@@ -58,8 +55,9 @@ def merge_resolutions(main_data, multiple_selection=False,
     #Read total number of images that will need to be upscaled
     max_count = 0
     for current_resolution in resolutions:
-        if multiple_selection:
-            max_count += len([n for n in main_data[current_resolution] if n])
+        if map_selection:
+            max_count += len(map_selection)
+            #max_count += len([n for n in main_data[current_resolution] if n])
         else:
             max_count += 1
     
@@ -68,10 +66,11 @@ def merge_resolutions(main_data, multiple_selection=False,
     total = 0
     
     #Upscale each resolution to the same level
+    highest_value = 0
+    lowest_value = 0
     for current_resolution in resolutions:
-        if multiple_selection:
-            array_list = [main_data[current_resolution][n] 
-                          for n in multiple_selection if main_data[current_resolution][n]]
+        if map_selection:
+            array_list = [main_data[m][current_resolution] for m in map_selection]
         else:
             array_list = [main_data[current_resolution]]
             
@@ -87,46 +86,17 @@ def merge_resolutions(main_data, multiple_selection=False,
                                          max_resolution[0], max_resolution[1],
                                          i, max_count))
 
-            #Try find the highest and lowest value
-            if _find_range:
-                all_values = set(data.values())
-                if not all_values:
-                    continue
-                _lowest_value = min(all_values)
-                if lowest_value is None or lowest_value > _lowest_value:
-                    lowest_value = _lowest_value
-                _highest_value = max(all_values)
-                if highest_value is None or highest_value < _highest_value:
-                    highest_value = _highest_value
-
-            #Build 2D array from data
-            new_data = []
-            width_range = range(current_resolution[0])
-            height_range = range(current_resolution[1])
-            for y in height_range:
-                new_data.append([])
-                for x in width_range:
-                    try:
-                        value = data[(x, y)]
-                        if session_start is not None:
-                            value = max(0, value - session_start)
-                        new_data[-1].append(value)
-                        total += value
-                    except KeyError:
-                        new_data[-1].append(0)
-                    else:
-                        count += 1
+            lowest_value = min(lowest_value, numpy.min(data))
+            highest_value = max(highest_value, numpy.max(data))
 
             #Calculate the zoom level needed
             zoom_factor = (max_resolution[1] / current_resolution[1],
                            max_resolution[0] / current_resolution[0])
-            numpy_arrays.append(upscale(numpy_array(new_data), zoom_factor))
+            numpy_arrays.append(upscale(numpy.array(data), zoom_factor))
     
-    if _find_range:
-        if session_start is not None:
-            highest_value -= session_start
-        return (lowest_value, highest_value), numpy_arrays
-    return numpy_arrays
+    if session_start is not None:
+        highest_value -= session_start
+    return (lowest_value, highest_value), numpy_arrays
     
     
 def convert_to_rgb(image_array, colour_range):
@@ -143,18 +113,17 @@ def convert_to_rgb(image_array, colour_range):
         num_processes = max_cores + num_processes
         if not num_processes:
             num_processes = 1
-        
+    
     if num_processes == 1:
         return _rgb_process_single(image_array, colour_range)        
     
     processes = range(num_processes)
     
     #Get actual image dimensions
-    height = len(image_array)
-    width = len(image_array[0])
+    height, width = image_array.shape
     
     #Figure how to split for each process
-    width_range = range(width)
+    width_range = tuple(range(width))
     height_per_process = height // num_processes
     heights = [[i * height_per_process, (i + 1) * height_per_process] 
                for i in range(num_processes)]
@@ -163,7 +132,7 @@ def convert_to_rgb(image_array, colour_range):
     q_send = Queue()
     q_recv = Queue()
     for i in processes:
-        height_range = range(heights[i][0], heights[i][1])
+        height_range = tuple(range(heights[i][0], heights[i][1]))
         q_send.put((i, width_range, height_range, colour_range, image_array))
         
         Process(target=_rgb_process_worker, args=(q_send, q_recv)).start()
@@ -183,7 +152,7 @@ def convert_to_rgb(image_array, colour_range):
     for i in processes:
         results += result_data[i]
     
-    return numpy_array(results, dtype='uint8')
+    return numpy.array(results, dtype='uint8')
 
     
 def _rgb_process_worker(q_recv, q_send):
@@ -191,6 +160,7 @@ def _rgb_process_worker(q_recv, q_send):
     This is a shortened version of _rgb_process_single meant for multiprocessing.
     """
     i, width_range, height_range, colour_range, image_array = q_recv.get()
+    
     result = [[]]
     for y in height_range:
         if result[-1]:
@@ -203,23 +173,15 @@ def _rgb_process_worker(q_recv, q_send):
 def _rgb_process_single(image_array, colour_range):
     """Turn each element in a 2D array to its corresponding colour."""
     
-    height_range = range(len(image_array))
-    width_range = range(len(image_array[0]))
+    height, width = image_array.shape
+    height_range = tuple(range(height))
+    width_range = tuple(range(width))
     
-    new_data = [[]]
-    total = len(image_array) * len(image_array[0])
-    count = 0
-    one_percent = int(round(total / 100))
+    array = numpy.array((4, width, height), create=True, dtype='uint8')
     for y in height_range:
-        if new_data[-1]:
-            new_data.append([])
         for x in width_range:
-            new_data[-1].append(colour_range[image_array[y][x]])
-            count += 1
-            if not count % one_percent:
-                _print('{}% complete ({} pixels)'.format(int(round(100 * count / total)), count))
-            
-    return numpy_array(new_data, dtype='uint8')
+            array[y][x] = colour_range[image_array[y][x]]
+    return array
 
 
 def arrays_to_heatmap(numpy_arrays, gaussian_size, clip):
@@ -230,11 +192,11 @@ def arrays_to_heatmap(numpy_arrays, gaussian_size, clip):
     
     #Add all arrays together
     _print('Merging arrays...')
-    merged_arrays = numpy_merge(numpy_arrays, 'add', 'float64')
+    merged_arrays = numpy.merge(numpy_arrays, 'add', 'float64')
     
     #Set to constant values
     _print('Flattening values...')
-    flattened = numpy_remap_to_range(merged_arrays)
+    flattened = numpy.remap_to_range(merged_arrays)
     
     #Blur the array
     if gaussian_size:
@@ -244,7 +206,7 @@ def arrays_to_heatmap(numpy_arrays, gaussian_size, clip):
         heatmap = flattened
     
     _print('Finding range limits...')
-    min_value = numpy_min(heatmap)
+    min_value = numpy.min(heatmap)
     
     #Lower the maximum value a little
     all_values = sorted(set(heatmap.ravel()))
@@ -256,7 +218,7 @@ def arrays_to_heatmap(numpy_arrays, gaussian_size, clip):
 def arrays_to_colour(colour_range, numpy_arrays):
     """Convert an array of floats or integers into an image object."""
 
-    max_array = numpy_merge(numpy_arrays, 'max')
+    max_array = numpy.merge(numpy_arrays, 'max')
     if max_array is None:
         return None
     
