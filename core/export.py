f33c2f9164b93ab900a93b75230b95f1c8afc0ff
Peter <Peter92@users.noreply.github.com>
2017-09-17 07:40:16 +0100

    Updated for new file format

--- core/export.py
+++ core/export.py
@@ -2,32 +2,23 @@ from __future__ import absolute_import
 
 from core.compatibility import range, get_items, _print
 from core.config import CONFIG
-from core.files import load_program
+from core.files import load_data
 from core.os import create_folder
+import core.numpy as numpy
 
 
 class ExportCSV(object):
     def __init__(self, profile, data=None):
         if data is None:
-            data = load_program(profile, _update_version=False)
+            data = load_data(profile, _update_version=False)
         self.profile = profile
         self.data = data
     
     def _generate(self, resolution, data):
         
-        if len(data) < CONFIG['GenerateCSV']['MinimumResPoints']:
+        if numpy.count(data) < CONFIG['GenerateCSV']['MinimumPoints']:
             return None
-        
-        #Build list
-        output = [['0'] * resolution[0] for _ in range(resolution[1])]
-        for k, v in get_items(data):
-            output[k[1]][k[0]] = str(v)
-        
-        #Add axis
-        output_axis = [[''] + [str(i) for i in range(resolution[0])]]
-        for i, row in enumerate(output):
-            output_axis.append([str(i)] + row)
-        
+        output = numpy.set_type(data, str)
         return '\n'.join(','.join(row) for row in output)
    
     def tracks(self, image_name):
@@ -37,7 +28,6 @@ class ExportCSV(object):
             CONFIG['GenerateImages']['_TempResolutionX'], CONFIG['GenerateImages']['_TempResolutionY'] = resolution
             
             result = self._generate(resolution, self.data['Maps']['Tracks'][resolution])
-            
             if result is not None:
                 file_name = image_name.generate('csv-tracks', reload=True)
                 create_folder(file_name)
@@ -49,23 +39,23 @@ class ExportCSV(object):
         mouse_buttons = ['LMB', 'MMB', 'RMB']
         
         _print('Generating CSV from clicks...')
-        for resolution in self.data['Maps']['Clicks']:
-            CONFIG['GenerateImages']['_TempResolutionX'], CONFIG['GenerateImages']['_TempResolutionY'] = resolution
         
-            for i, mb_clicks in enumerate(self.data['Maps']['Clicks'][resolution]):
-                if mb_clicks:
-                    
-                    CONFIG['GenerateHeatmap']['_MouseButtonLeft'] = i == 0
-                    CONFIG['GenerateHeatmap']['_MouseButtonMiddle'] = i == 1
-                    CONFIG['GenerateHeatmap']['_MouseButtonRight'] = i == 2
-        
-                    result = self._generate(resolution, mb_clicks)
+        for i, mouse_button in enumerate(self.data['Maps']['Click']['Single']):
+            CONFIG['GenerateHeatmap']['_MouseButtonLeft'] = i == 0
+            CONFIG['GenerateHeatmap']['_MouseButtonMiddle'] = i == 1
+            CONFIG['GenerateHeatmap']['_MouseButtonRight'] = i == 2
             
-                    if result is not None:
-                        file_name = image_name.generate('csv-clicks', reload=True)
-                        create_folder(file_name)
-                        with open(file_name, 'w') as f:
-                            f.write(result)
+            for resolution, array in get_items(self.data['Maps']['Click']['Single'][mouse_button]):
+                CONFIG['GenerateImages']['_TempResolutionX'], CONFIG['GenerateImages']['_TempResolutionY'] = resolution
+            
+                result = self._generate(resolution, array)
+                if result is not None:
+                    file_name = image_name.generate('csv-clicks', reload=True)
+                    create_folder(file_name)
+                    with open(file_name, 'w') as f:
+                        f.write(result)
+            
+        
     
     def keyboard(self, image_name):
         
