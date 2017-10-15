bba15f674746e408e67970598f1d51ed8f0be824
Peter <Peter92@users.noreply.github.com>
2017-10-15 11:49:23 +0100

    Fallback to normal scipy if error

--- core/image/scipy/__init__.py
+++ core/image/scipy/__init__.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import
 
-#Took only the necessary functions from scipy, as to not have a huge filesize
-from core.image.scipy.gaussian import gaussian_filter
-from core.image.scipy.zoom import zoom
-
+try:
+    from core.image.scipy.gaussian import gaussian_filter
+    from core.image.scipy.zoom import zoom
+except ImportError: #DLL load failed: %1 is not a valid Win32 application - maybe issue with 32 bit python, needs testing
+    from scipy.ndimage.filters import gaussian_filter
+    from scipy.ndimage.interpolation import zoom
+    
 
 def blur(array, size):
     return gaussian_filter(array, sigma=size)
