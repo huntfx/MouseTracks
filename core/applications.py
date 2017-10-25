42cd4959115e806976f733689d996d6d20597d74
Peter <Peter92@users.noreply.github.com>
2017-10-25 17:58:00 +0100

    Fully rewrote for improved applist format

--- core/applications.py
+++ core/applications.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import time
 
+from core.compatibility import get_items
 from core.config import CONFIG
 from core.constants import APP_LIST_URL
 from core.notify import *
@@ -9,154 +10,188 @@ from core.os import get_running_processes, WindowFocus
 from core.internet import get_url_contents
 
 
+RECOGNISED_EXTENSIONS = ['exe', 'bin', 'app', 'scr', 'com']
+
 APP_LIST_PATH = format_file_path(CONFIG['Paths']['AppList'])
-
-_DEFAULT_TEXT = [
-    '// Type any apps you want to be tracked here.',
-    '// Two separate apps may have the same name, and will be tracked under the same file.',
-    '// Put each app on a new line, in the format "MyGame.exe: Game Name".'
-    ' The executable file is case sensitive.',
-    '// Alternatively if the app is already named with the correct name'
-    ', "MyGame.exe" by itself will use "MyGame" as its name.',
-    '// In the case of a game using a generic filename,'
-    ' you can type "Window Name || Game Name" instead.',
-    ''
-]
-
-ALLOWED_EXTENSIONS = ['.exe', '.bin', '.app', '.scr', '.com']
 
 _ENCODINGS = [''.join(chr(i) for i in (239, 187, 191))]
 
+_DEFAULT_TEXT = [
+    '// Add any applications you want to be tracked here.',
+    '// Two separate applications can have the same name,'
+    ' and will be tracked under the same file.',
+    '// Put each app on a new line, in the format "MyGame.exe: Game Name".'
+    ' The executable file is case sensitive.',
+    '// You may also limit it to a certain window name,'
+    ' for example if the game uses a generic executable name.',
+    '// This would work like "Play.exe[MyGame]: Game Name".',
+    '// If the executable or window name is the same as the game name,'
+    ' you only need to provide that.'
+]
 
-def _format_app_text(app, friendly_name=None, allow_non_executable=True, _is_exe=True):
 
-    app = app.strip()
-    if not app:
-        return None
-    
-    #Remove encoding from start of file
-    for i in _ENCODINGS:
-        if app.startswith(i):
-            app = app[len(i):]
-            break
+class AppList(object):
+    def __init__(self, path=APP_LIST_PATH):
+        self.path = path
 
-    #Ignore comments
-    if any(app.startswith(i) for i in ('#', ';', '//')):
-        return None
+        extensions = ['.'+i for i in RECOGNISED_EXTENSIONS]
+        self.extensions = {i: {'len': len(i), ':': i+':', '[': i+'['}
+                           for i in extensions}
         
-    #Detect different extensions
-    lowercase = app.lower()
-    found_extension = False
-    split_value = ':'
-    for i in ALLOWED_EXTENSIONS:
-        if i in lowercase:
-            found_extension = True
-            ext_len = len(lowercase.split(i)[0])
-            app_name = '{}.{}'.format(app[:ext_len], app[ext_len + 1:ext_len + 4])
-            break
-    
-    #Doesn't match any extension
-    if not found_extension:
-        split_value = '||'
-        if allow_non_executable:
-            ext_len = len(lowercase.split(split_value, 1)[0])
-            app_name = app[:ext_len].strip()
+        self.refresh()
+
+    def _read(self, path=None, url=None):
+        """Parse an application list and return a dictionary.
+
+        Accepted formats:
+            MyGame.exe
+            MyGame.exe: Game Name
+            MyGame.exe[Window Name]
+            MyGame.exe[Window Name]: Game Name
+        """
+        if path is None:
+            if url is None:
+                return {}
+            try:
+                lines = get_url_contents(path).split('\n')
+            except AttributeError:
+                return {}
+                
         else:
-            return None
-    
-    #Determine if name has been provided in file, or generate if not
-    if friendly_name is None:
-        try:
-            friendly_name = app[ext_len:].split(split_value, 1)[1].strip()
-        except IndexError:
-            friendly_name = app[:ext_len].strip()
-        return (app_name, friendly_name, found_extension)
-    
-    #Format line to save
-    else:
-        if app_name[:ext_len] == friendly_name:
-            return app_name
-        else:
-            formatting = '{}: {}' if _is_exe else '{} || {}'
-            return formatting.format(app_name, friendly_name)
+            with open(path, 'r') as f:
+                lines = [i.strip() for i in f.readlines()]
+            
+        for marker in _ENCODINGS:
+            if lines[0].startswith(marker):
+                lines[0] = lines[0][len(marker):]
+                break
 
-    
-def _format_app_list(app_list, combine=True):
-    """Take a list of text inputs and sort them into a dictionary."""
-    if isinstance(app_list, str):
-        app_list = app_list.splitlines()
+        executable_files = {}
+        for line in lines:
+                        
+            #Ignore comments
+            if line.startswith('//'):
+                continue
+
+            #Check for each extensions
+            no_space = line.replace(' ', '').lower()
+            for ext in self.extensions:
+                if ext in line:
+
+                    #In the format "MyGame.exe: Game Name"
+                    if self.extensions[ext][':'] in no_space:
+                        executable_name, game_name = line.split(':', 1)
+                        window_name = None
+
+                    #In the format "MyGame.exe[Window Name]"
+                    #or "MyGame.exe[Window Name]: Game Name"
+                    elif self.extensions[ext]['['] in no_space:
+                        executable_name, remaining = line.split('[', 1)
+                        window_name, remaining = remaining.split(']', 1)
+                        if remaining.strip().startswith(':'):
+                            game_name = remaining.split(':', 1)[1]
+                        else:
+                            game_name = window_name
+
+                    #In the format "MyGame.exe"
+                    elif no_space.endswith(ext):
+                        executable_name = line
+                        window_name = None
+                        game_name = line[:-self.extensions[ext]['len']]
+                    else:
+                        continue
+                    
+                    try:
+                        executable_files[executable_name.strip()][window_name] = game_name.strip()
+                    except KeyError:
+                        executable_files[executable_name.strip()] = {window_name: game_name.strip()}
+                    
+        return executable_files
+
+    def refresh(self):
+        """Get data from the file."""
+        self.data = self._read(self.path)
+
+    def save(self, path=None):
+        """Save the sorted application list."""
+        result = []
+        for executable, names in get_items(self.data):
+            for ext in self.extensions:
+                if executable.endswith(ext):
+                    for window_name, app_name in get_items(names):
+                        if window_name is None:
+                            if app_name == executable[:-self.extensions[ext]['len']]:
+                                result.append(executable)
+                            else:
+                                result.append('{}: {}'.format(executable, app_name))
+                        else:
+                            if window_name == app_name:
+                                result.append('{}[{}]'.format(executable, window_name))
+                            else:
+                                result.append('{}[{}]: {}'.format(executable, window_name, app_name))
+                    break
+        result = '\r\n'.join(_DEFAULT_TEXT + [''] + sorted(result, key=str.lower))
         
-    applications = {}
-    names = {}
-    for app in app_list:
-        
-        app_info = _format_app_text(app.strip())
-        if app_info is not None:
-            app_name, friendly_name, is_executable = app_info
-            if is_executable or combine:
-                applications[app_name] = friendly_name
+        if path is None:
+            path = self.path
+        with open(path, 'w') as f:
+            f.write(result)
+        return result
+
+    def update(self, url):
+        """Update application list from URL.
+        Does not overwrite any values.
+        """
+        url_data = self._read(url=url)
+        if not url_data:
+            return False
+        for executable, names in get_items(url_data):
+            if executable not in self.data:
+                self.data[executable] = url_data[executable]
             else:
-                names[app_name] = friendly_name
-    
-    if combine:
-        return applications
-    else:
-        return applications, names
-
-
-def read_app_list(application_path=APP_LIST_PATH, allow_write=False, combine=True):
-    """Read the application list file.
-    Optionally write a new one if it doesn't exist.
-    """
-    try:
-        with open(application_path, 'r') as f:
-            lines = f.readlines()
-    except IOError:
-        lines = _DEFAULT_TEXT + ['// MyGame.exe: Game Name']
-        if allow_write:
-            with open(application_path, 'w') as f:
-                f.write('\r\n'.join(lines))
-
-    return _format_app_list(lines, combine=combine)
-
-    
-def download_app_list():
-    """Download from the online application list.
-    Requires the config to be set to allow internet access.
-    """
-    if not CONFIG['Internet']['Enable']:
-        return None
-    application_list_contents = get_url_contents(APP_LIST_URL)
-    if application_list_contents:
-        return _format_app_list(application_list_contents)
-    return None
-
-
-def update_app_list(applications, downloaded_applications=None):
-    """Combine the current application list with the downloaded one."""
-    if downloaded_applications is None:
-        downloaded_applications = download_app_list()
-    if downloaded_applications:
-        for k, v in downloaded_applications.iteritems():
-            if k not in applications:
-                applications[k] = v
+                for name in names:
+                    if name not in self.data[executable]:
+                        self.data[executable][name] = names[name]
         return True
-    return False
-    
+        
+    def __getitem__(self, key):
+        return self.data[key]
+        
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
+
+    def __contains__(self, key):
+        return key in self.data
+
+    def __iter__(self):
+        return self.data.keys().__iter__()
+
+    def __str__(self):
+        return str(self.data)
+
+    def __nonzero__(self):
+        return bool(self.data)
+
+    def __bool__(self):
+        return bool(self.data)
+
+    def pop(self, n):
+        return self.data.pop(n)
+        
 
 class RunningApplications(object):
     """Detect which applications are currently running."""
 
     def __init__(self, application_path=APP_LIST_PATH, queue=None):
         self.q = queue
+        self.applist = AppList(application_path)
         self.refresh()
-        self.application_path = application_path
         self.reload_file()
 
     def reload_file(self):
-    
-        self.applications, self.names = read_app_list(self.application_path, allow_write=True, combine=False)
-        
         #Download from the internet and combine with the current list
         last_updated = CONFIG['SavedSettings']['AppListUpdate']
         update_frequency = CONFIG['Internet']['UpdateApplications'] * 60
@@ -166,17 +201,16 @@ class RunningApplications(object):
         
         #Only update if requirements are met
         next_update = time.time() - update_frequency
-        if not self.applications or not last_updated or last_updated < next_update:
+        if not self.applist or not last_updated or last_updated < next_update:
         
             if self.q is not None:
                 NOTIFY(APPLIST_UPDATE_START)
                 NOTIFY.send(self.q)
-            
-            if update_app_list(self.applications):
-            
+                
+            if self.applist.update(APP_LIST_URL):
                 CONFIG['SavedSettings']['AppListUpdate'] = int(time.time())
                 CONFIG.save()
-                self.save_file()
+                self.applist.save()
                 
                 if self.q is not None:
                     NOTIFY(APPLIST_UPDATE_SUCCESS)
@@ -185,57 +219,50 @@ class RunningApplications(object):
                     NOTIFY(APPLIST_UPDATE_FAIL)
                     
             NOTIFY.send(self.q)
-    
+        
     def refresh(self):
-        self.processes = get_running_processes()
-        if WindowFocus is not None:
-            self.focus = WindowFocus()
-            self.focused_app = self.focus.exe()
-            self.focused_name = self.focus.name()
-        else:
+        """Get list of currently running programs and focused window."""
+        if WindowFocus is None:
             self.focus = None
-    
-    def save_file(self):
-        lines = _DEFAULT_TEXT
-        
-        for app_info in sorted(self.applications.keys() + self.names.keys(), key=lambda s: s.lower()):
-        
-            try:
-                friendly_name = self.names[app_info]
-                _is_exe = False
-            except KeyError:
-                friendly_name = self.applications[app_info]
-                _is_exe = True
-            
-            new_line = _format_app_text(app_info, friendly_name, _is_exe=_is_exe)
-            if new_line is not None:
-                lines.append(new_line)
-        
-        with open(self.application_path, 'w') as f:
-            f.write('\n'.join(lines))
-    
+            self.processes = get_running_processes()
+        else:
+            self.focus = WindowFocus()
+            self.focused_exe = self.focus.exe()
+            self.focused_name = self.focus.name()
 
     def check(self):
-        """Check for any applications in the list that are currently loaded.
-        Check if any of them match the currently focused window (if applicable),
-        or choose the one with the highest ID as it'll likely be the most recent one.
-        """
-        if self.focus is not None:
-            try:
-                return (self.names[self.focused_name], self.focused_app)
-            except KeyError:
-                pass
-            try:
-                return (self.applications[self.focused_app], self.focused_app)
-            except KeyError:
-                pass
+        """Return the name and executable of a running application."""
+        #Get most recently loaded application
+        if self.focus is None:
+            
+            matching_applications = {self.processes[app]: app
+                                     for app in self.applist
+                                     if app in self.processes}
+            
+            for index in sorted(matching_applications.keys())[::-1]:
+                loaded_exe = matching_applications[index]
+                names = self.applist[loaded_exe]
+                try:
+                    return names[None], loaded_exe
+
+                #Only fallback to window name if it is the only entry for that application
+                except KeyError:
+                    if len(names) == 1 or len(set(names.values())) == 1:
+                        return names[names.keys()[0]], loaded_exe
             return None
+
+        #Get currently focused application
         else:
-            matching_applications = {}
-            for application in self.applications:
-                if application in self.processes:
-                    matching_applications[self.processes[application]] = application
-            if not matching_applications:
-                return None
-            latest_application = matching_applications[max(matching_applications.keys())]
-            return (self.applications[latest_application], latest_application)
+            if self.focused_exe in self.applist:
+                names = self.applist[self.focused_exe]
+                try:
+                    return names[self.focused_name], self.focused_exe
+                except KeyError:
+                    try:
+                        return names[None], self.focused_exe
+                    except KeyError:
+                        return None
+    
+    def save_file(self):
+        self.applist.save()
+
