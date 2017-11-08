e2a78dc0db63f565a42ca6920d9b335b12d6bb1b
Peter <Peter92@users.noreply.github.com>
2017-11-08 16:30:00 +0000

    Elevate process to administrator

--- start_tracking.py
+++ start_tracking.py
@@ -1,10 +1,15 @@
 from __future__ import absolute_import
 from multiprocessing import freeze_support
 
+from core.config import CONFIG
 from core.track import start_tracking
+from core.os import elevate
 
 
 if __name__ == '__main__':
-    
     freeze_support()
+    
+    if CONFIG['Advanced']['RunAsAdministrator']:
+        elevate()
+        
     start_tracking()
