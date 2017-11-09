f44631364c87c9d5228e7f8128995e854c79bffc
Peter <Peter92@users.noreply.github.com>
2017-11-09 14:24:24 +0000

    Simple but important functions

---
+++ core/base.py
@@ -0,0 +1,42 @@
+from __future__ import division, absolute_import
+from re import sub
+
+from core.os import get_documents_path, read_env_var
+
+
+def format_name(name, extra_chars=''):
+    """Remove any invalid characters for file name."""
+    return sub('[^A-Za-z0-9{}]+'.format(extra_chars), '', name).lower()
+    
+
+def format_file_path(path):
+    """Process an input path and follow any environment variables."""
+    
+    #Set up any custom environment variables here
+    custom_paths = {
+        '%DOCUMENTS%': get_documents_path()
+    }
+    
+    repeat = False
+    parts = path.replace('\\', '/').rstrip('/').split('/')
+    file_name = parts.pop(-1) if '.' in parts[-1] else None
+    
+    #Process each part separately
+    for i, part in enumerate(parts):
+        try:
+            parts[i] = custom_paths[part]
+        except KeyError:
+            env_var = read_env_var(part)
+            if env_var is not None:
+                parts[i] = env_var
+        else:
+            if '%' in parts[i]:
+                repeat = True
+                
+    if file_name is not None:
+        parts.append(file_name)
+    final_path = '/'.join(i.replace('\\', '/') for i in parts if i)
+    
+    if repeat:
+        return format_file_path(final_path)
+    return final_path
