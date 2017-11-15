2b6a4f369b20b9b18c4058bc2e43600edd84d7d6
Peter <Peter92@users.noreply.github.com>
2017-11-15 19:51:54 +0000

    Added list of keys

--- core/os/__init__.py
+++ core/os/__init__.py
@@ -85,6 +85,94 @@ def join_path(path, create=False):
     
 def open_folder(path):
     os.startfile(path)
+
+
+KEYS = {
+    'THUMB1': 5,
+    'THUMB2': 6,
+    'BACK': 8,
+    'TAB': 9,
+    'CLEAR': 12,
+    'RETURN': 13,
+    'PAUSE': 19,
+    'CAPSLOCK': 20,
+    'ESC': 27,
+    'SPACE': 32,
+    'PGUP': 33,
+    'PGDOWN': 34,
+    'END': 35,
+    'HOME': 36,
+    'LEFT': 37,
+    'UP': 38,
+    'RIGHT': 39,
+    'DOWN': 40,
+    'INSERT': 45,
+    'DELETE': 46,
+    'LWIN': 91,
+    'RWIN': 92,
+    'MENU': 93,
+    'NUM0': 96,
+    'NUM1': 97,
+    'NUM2': 98,
+    'NUM3': 99,
+    'NUM4': 100,
+    'NUM5': 101,
+    'NUM6': 102,
+    'NUM7': 103,
+    'NUM8': 104,
+    'NUM9': 105,
+    'MULTIPLY': 106,
+    'ADD': 107,
+    'SUBTRACT': 109,
+    'DECIMAL': 110,
+    'DIVIDE': 111,
+    'F1': 112,
+    'F2': 113,
+    'F3': 114,
+    'F4': 115,
+    'F5': 116,
+    'F6': 117,
+    'F7': 118,
+    'F8': 119,
+    'F9': 120,
+    'F10': 121,
+    'F11': 122,
+    'F12': 123,
+    'F13': 124,
+    'F14': 125,
+    'F15': 126,
+    'F16': 127,
+    'F17': 128,
+    'F18': 129,
+    'F19': 130,
+    'F20': 131,
+    'F21': 132,
+    'F22': 133,
+    'F23': 134,
+    'F24': 135,
+    'NUMLOCK': 144,
+    'SCROLLLOCK': 145,
+    'LSHIFT': 160,
+    'RSHIFT': 161,
+    'LCTRL': 162,
+    'RCTRL': 163,
+    'LALT': 164,
+    'RALT': 165,
+    'COLON': 186,
+    'EQUALS': 187,
+    'COMMA': 188,
+    'UNDERSCORE': 189,
+    'PERIOD': 190,
+    'FORWARDSLASH': 191,
+    'AT': 192,
+    'LBRACKET': 219,
+    'BACKSLASH': 220,
+    'RBRACKET': 221,
+    'HASH': 222,
+    'TILDE': 223
+}
+for c in list('ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'):
+    KEYS[c] = ord(c)
     
 
 #Load in modules from operating system
@@ -110,8 +198,8 @@ elif OPERATING_SYSTEM == 'Darwin':
 else:
     raise ImportError('unknown operating system: "{}"'.format(OPERATING_SYSTEM))
 
-
-def _add_placeholders(variables):
+#Import placeholders if the function doesn't exist in the namespace
+def _add_placeholders(variables, functions_only=False):
     """Use placeholder functions if the counterpart doesn't exist."""
     count = 0
     for f_name in dir(placeholders):
@@ -119,13 +207,12 @@ def _add_placeholders(variables):
             variables[f_name]
         except KeyError:
             f = getattr(placeholders, f_name, None)
-            if callable(f):
+            if not functions_only or callable(f):
                 variables[f_name] = f
                 count += 1
     return count
     
 PLACEHOLDER_COUNT = _add_placeholders(locals())
-
 
 #Check the functions exist
 try:
