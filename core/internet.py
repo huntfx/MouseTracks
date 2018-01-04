94a89a690537b0c6fcf2530c3710e4926b641516
Peter <justanotherguy3571@yahoo.co.uk>
2018-01-04 18:37:30 +0000

    Python 3 byte type fix

--- core/internet.py
+++ core/internet.py
@@ -14,6 +14,6 @@ except ImportError:
 def get_url_contents(url):
     """Get data from a URL."""
     try:
-        return urllib2.urlopen(url).read()
+        return str(urllib2.urlopen(url).read())
     except (urllib2.URLError, urllib2.HTTPError):
         return None
