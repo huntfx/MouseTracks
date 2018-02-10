783af4dc54a269e8c983c625d967c5595e9e7aa9
Peter <justanotherguy3571@yahoo.co.uk>
2018-02-10 20:13:07 +0000

    Use function to ask y/n

--- applist_update.py
+++ applist_update.py
@@ -9,15 +9,15 @@ from core.applications import AppList
 from core.compatibility import input
 from core.config import CONFIG
 from core.constants import APP_LIST_FILE
+from core.input import yes_or_no
 
 
 if __name__ == '__main__':
 
     if not CONFIG['Internet']['Enable']:
-        choice = input('Internet access is disabled. Would you like to update from the online {}? (y/n) '.format(APP_LIST_FILE)).lower()
-        if choice.lower().startswith('y') and 'no' not in choice:
+        if yes_or_no('Internet access is disabled. Would you like to update from the online {}?'.format(APP_LIST_FILE)):
             CONFIG['Internet']['Enable'] = True
-
+            
     applist = AppList()
     while True:
         if applist.update():
@@ -25,6 +25,5 @@ if __name__ == '__main__':
             input('Finished, press enter to quit.')
             break
         else:
-            choice = input('Failed to update. Would you like to retry? (y/n) ').lower()
-            if 'no' in choice or not choice.lower().startswith('y'):
+            if not yes_or_no('Failed to update. Would you like to retry?'):
                 break
