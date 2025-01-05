57c3889fa8aa1c6c6934f7fdd7ec14e5a380e8e2
Peter Hunt <peter@huntfx.uk>
2025-01-05 18:49:45 +0000

    Add constants file

---
+++ mousetracks2/constants.py
@@ -0,0 +1,21 @@
+DEFAULT_APPLICATION_NAME = 'Main'
+
+UPDATES_PER_SECOND = 60
+
+DOUBLE_CLICK_MS = 500
+"""Maximum time in ms where a double click is valid."""
+
+DOUBLE_CLICK_TOL = 8
+"""Maximum pixels where a double click is valid."""
+
+COMPRESSION_THRESHOLD = 425000  # Max: 2 ** 64 - 1
+"""How many ticks to trigger track compression."""
+
+COMPRESSION_FACTOR = 1.1
+"""How much to compress tracks by."""
+
+INACTIVITY_MS = 300000
+"""Time in ms before inactive."""
+
+RADIAL_ARRAY_SIZE = 2048
+"""Size to use for gamepad radial arrays."""
