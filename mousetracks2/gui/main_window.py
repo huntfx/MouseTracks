huntfx/MouseTracks

commit 7965b7995d3183a016554ccbf27c9baa37b2f3eb

Improve sampling options
Allows the sampling level of 0, and allows updating the sampling of the preview render.

mousetracks2/gui/main_window.py
@@ -283,6 +283,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.custom_height.valueChanged.connect(self.render_resolution_value_changed)
         self.ui.enable_custom_width.stateChanged.connect(self.render_resolution_state_changed)
         self.ui.enable_custom_height.stateChanged.connect(self.render_resolution_state_changed)
+        self.ui.thumbnail_sampling.valueChanged.connect(self.thumbnail_sample_value_changed)
         self.ui.applist_reload.clicked.connect(self.reload_applist)
         self.ui.track_mouse.stateChanged.connect(self.handle_delete_button_visibility)
         self.ui.track_keyboard.stateChanged.connect(self.handle_delete_button_visibility)
@@ -348,6 +349,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Trigger initial setup
         self.profile_changed(0)
+        self.ui.show_advanced.setChecked(False)
 
         # Set tip
         tips = ['tip_tracking']
@@ -430,7 +432,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.clipping.setValue(self.clipping)
         self.ui.blur.setValue(self.blur)
         self.ui.linear.setChecked(self.linear)
-        self._set_advanced_visibility()
 
         self.pause_colour_change = False
 
@@ -929,6 +930,11 @@ class MainWindow(QtWidgets.QMainWindow):
         if not self.ui.lock_aspect.isChecked():
             self.request_thumbnail()
 
+    @QtCore.Slot(int)
+    def thumbnail_sample_value_changed(self, value: int) -> None:
+        """Update the thumbnail when the resampling mode is changed."""
+        self.request_thumbnail()
+
     @QtCore.Slot(QtCore.Qt.CheckState)
     def toggle_auto_switch_profile(self, state: QtCore.Qt.CheckState) -> None:
         """Switch to the current profile when auto switch is checked."""
@@ -1026,12 +1032,12 @@ class MainWindow(QtWidgets.QMainWindow):
                                                    width=width, height=height, lock_aspect=aspect is None,
                                                    profile=profile, file_path=None,
                                                    colour_map=self.render_colour, padding=self.padding,
+                                                   sampling=self.ui.thumbnail_sampling.value(),
                                                    contrast=self.contrast, clipping=self.clipping,
                                                    blur=self.blur, linear=self.linear,
                                                    show_left_clicks=self.ui.show_left_clicks.isChecked(),
                                                    show_middle_clicks=self.ui.show_middle_clicks.isChecked(),
-                                                   show_right_clicks=self.ui.show_right_clicks.isChecked(),
-                                                   ))
+                                                   show_right_clicks=self.ui.show_right_clicks.isChecked()))
         return True
 
     @QtCore.Slot(QtCore.QSize)
@@ -1213,8 +1219,8 @@ class MainWindow(QtWidgets.QMainWindow):
                 height, width, channels = message.array.shape
                 failed = width == height == 0
 
-                target_height = int(height / message.request.sampling)
-                target_width = int(width / message.request.sampling)
+                target_height = int(height / (message.request.sampling or 1))
+                target_width = int(width / (message.request.sampling or 1))
 
                 # Draw the new pixmap
                 if message.request.file_path is None:
@@ -1240,7 +1246,7 @@ class MainWindow(QtWidgets.QMainWindow):
                         image = QtGui.QImage(message.array.data, width, height, stride, image_format)
 
                         # Scale the QImage to fit the pixmap size
-                        scaled_image = image.scaled(target_width, target_height, QtCore.Qt.AspectRatioMode.KeepAspectRatio)
+                        scaled_image = image.scaled(target_width, target_height, QtCore.Qt.AspectRatioMode.KeepAspectRatio, QtCore.Qt.TransformationMode.SmoothTransformation)
                         self.ui.thumbnail.set_pixmap(scaled_image)
 
                     self.pause_redraw -= 1
@@ -2129,7 +2135,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.main_layout.setContentsMargins(QtCore.QMargins(0, 0, 0, 0) if full_screen else self._margins_main)
         self.ui.render_layout.setContentsMargins(QtCore.QMargins(0, 0, 0, 0) if full_screen else self._margins_render)
 
-    def _set_advanced_visibility(self):
+    @QtCore.Slot(bool)
+    def toggle_advanced_options(self, enabled: bool) -> None:
         """Set the visibility of render option widgets."""
         show_advanced = self.ui.show_advanced.isChecked()
 
@@ -2145,6 +2152,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self._buddies[self.ui.contrast].setVisible(show_advanced and not is_keyboard)
         self.ui.sampling.setVisible(show_advanced)
         self._buddies[self.ui.sampling].setVisible(show_advanced)
+        self.ui.thumbnail_sampling.setVisible(show_advanced)
+        self._buddies[self.ui.thumbnail_sampling].setVisible(show_advanced)
         self.ui.padding.setVisible(show_advanced and not is_keyboard)
         self._buddies[self.ui.padding].setVisible(show_advanced and not is_keyboard)
         self.ui.clipping.setVisible(show_advanced and not is_keyboard)
@@ -2154,11 +2163,6 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.linear.setVisible(show_advanced and not is_keyboard)
 
         self.ui.resolution_group.setVisible(show_advanced and not is_keyboard)
-
-    @QtCore.Slot(bool)
-    def toggle_advanced_options(self, enabled: bool) -> None:
-        """Update visibility of widgets when the advanced button is toggled."""
-        self._set_advanced_visibility()
 
     def notify(self, message: str) -> None:
         """Show a notification.

mousetracks2/render.py
@@ -72,8 +72,11 @@ def gaussian_size(width, height, multiplier: float = 0.0125):
     return min(width, height) * multiplier
 
 
-def array_rescale(array: np.typing.ArrayLike, target_width: int, target_height: int) -> np.ndarray:
-    """Rescale the array with the correct filtering."""
+def array_rescale(array: np.typing.ArrayLike, target_width: int, target_height: int, sampling: int) -> np.ndarray:
+    """Rescale the array with the correct filtering.
+
+    If sampling is set, then the downscaling is disabled.
+    """
     array = np.asarray(array)
     input_height, input_width = array.shape
 
@@ -82,7 +85,7 @@ def array_rescale(array: np.typing.ArrayLike, target_width: int, target_height: 
         return np.asarray(array)
 
     # Upscale without blurring
-    if target_height > input_height or target_width > input_width:
+    if sampling:
         zoom_factor = (target_height / input_height, target_width / input_width)
         return ndimage.zoom(array, zoom_factor, order=0)
 
@@ -129,8 +132,7 @@ def generate_colour_lookup(*colours: tuple[int, ...], steps: int = 256) -> np.nd
 
 def render(colour_map: str, positional_arrays: dict[tuple[int, int], list[np.typing.ArrayLike]],
            width: int | None = None, height: int | None = None, sampling: int = 1, lock_aspect: bool = True,
-           linear: bool = False, blur: float = 0.0, contrast: float = 1.0,
-           clipping: float = 0.0) -> np.ndarray:
+           linear: bool = False, blur: float = 0.0, contrast: float = 1.0, clipping: float = 0.0) -> np.ndarray:
     """Combine a group of arrays into a single array for rendering.
 
     Parameters:
@@ -167,14 +169,14 @@ def render(colour_map: str, positional_arrays: dict[tuple[int, int], list[np.typ
     if not width or not height:
         raise EmptyRenderError
 
-    scale_width = width * sampling
-    scale_height = height * sampling
+    scale_width = width * (sampling or 1)
+    scale_height = height * (sampling or 1)
 
     # Rescale the arrays to the target size and combine them
     combined_arrays: dict[tuple[int, int], np.ndarray] = {}
     for pos, arrays in positional_arrays.items():
         if arrays:
-            rescaled = [array_rescale(array, scale_width, scale_height) for array in arrays]
+            rescaled = [array_rescale(array, scale_width, scale_height, sampling) for array in arrays]
             combined_arrays[pos] = np.maximum.reduce(rescaled)
         else:
             combined_arrays[pos] = np.zeros([scale_height, scale_width], dtype=np.uint8)

commit a96925c9f9cceabfa78fca1e857e03d5427657f8

Fix settings not updating
Fixes issue introduced in 7965b7995d3183a016554ccbf27c9baa37b2f3eb.

mousetracks2/gui/main_window.py
@@ -434,6 +434,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.clipping.setValue(self.clipping)
         self.ui.blur.setValue(self.blur)
         self.ui.linear.setChecked(self.linear)
+        self.toggle_advanced_options(self.ui.show_advanced.isChecked())
 
         self.pause_colour_change = False
 
@@ -2151,10 +2152,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.render_layout.setContentsMargins(QtCore.QMargins(0, 0, 0, 0) if full_screen else self._margins_render)
 
     @QtCore.Slot(bool)
-    def toggle_advanced_options(self, enabled: bool) -> None:
+    def toggle_advanced_options(self, show_advanced: bool) -> None:
         """Set the visibility of render option widgets."""
-        show_advanced = self.ui.show_advanced.isChecked()
-
         is_click = self.render_type in (ipc.RenderType.SingleClick, ipc.RenderType.DoubleClick, ipc.RenderType.HeldClick)
         is_thumbstick = self.render_type in (ipc.RenderType.Thumbstick_Time, ipc.RenderType.Thumbstick_Speed, ipc.RenderType.Thumbstick_Heatmap)
         is_keyboard = self.render_type == ipc.RenderType.Keyboard