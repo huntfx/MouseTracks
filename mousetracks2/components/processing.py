huntfx/MouseTracks

commit 50a0cd8f0d80f36928451e5b3eed95420f7c4c82

Internally sanitise profile names
Without this, it's possible to load two of the same profile at the same time with slightly different names, which would result in overwriting files.

mousetracks2/components/processing.py
@@ -58,7 +58,7 @@ class Processing(Component):
         self.previous_monitor = None
 
         # Load in the default profile
-        self.all_profiles: dict[str, TrackingProfile] = TrackingProfileLoader()
+        self.all_profiles = TrackingProfileLoader()
         self._current_application = Application('', [])
         self.current_application = Application(DEFAULT_PROFILE_NAME, [])
 

mousetracks2/file.py
@@ -4,6 +4,7 @@ import re
 import time
 import zipfile
 from collections import defaultdict
+from collections.abc import MutableMapping
 from dataclasses import dataclass, field
 from typing import Any, Generic, Iterator, Self, Sequence, Type, TypeVar
 from uuid import uuid4
@@ -662,17 +663,47 @@ class TrackingProfile:
         self.is_modified = True
 
 
-class TrackingProfileLoader(dict[str, TrackingProfile]):
+class TrackingProfileLoader(MutableMapping):
     """Act like a defaultdict to load data if available."""
 
-    def __missing__(self, profile_name: str) -> TrackingProfile:
-        """Load in any missing data or create a new profile."""
+    def __init__(self, max_profiles: int = 5):
+        self.max_profiles = max_profiles
+        self._profiles: dict[str, TrackingProfile] = {}
+
+    def __setitem__(self, profile_name: str, profile: TrackingProfile) -> None:
+        sanitised = santise_profile_name(profile_name)
+        self._profiles[sanitised] = profile
+
+    def __getitem__(self, profile_name: str) -> TrackingProfile:
+        """Load or get a profile."""
+        sanitised = santise_profile_name(profile_name)
+        try:
+            return self._profiles[sanitised]
+        except KeyError:
+            self._profiles[sanitised] = self._load_or_create_profile(profile_name)
+        return self._profiles[sanitised]
+
+    def __delitem__(self, profile_name: str) -> None:
+        sanitised = santise_profile_name(profile_name)
+        del self._profiles[sanitised]
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self._profiles)
+
+    def __len__(self) -> int:
+        return len(self._profiles)
+
+    def _load_or_create_profile(self, profile_name: str) -> TrackingProfile:
+        """Load in any missing data or create a new profile.
+        This is in the place of `__missing__`, as the profile name gets
+        sanitised before it reaches that point.
+        """
         filename = get_filename(profile_name)
         if os.path.exists(filename):
-            profile = self[profile_name] = TrackingProfile.load(filename)
+            profile = self._profiles[profile_name] = TrackingProfile.load(filename)
         else:
-            profile = self[profile_name] = TrackingProfile()
-        self._autoUnload(ignore=profile_name)
+            profile = self._profiles[profile_name] = TrackingProfile()
+        self._evict(keep_loaded=santise_profile_name(profile_name))
 
         # Update the data
         profile.name = profile_name
@@ -687,24 +718,30 @@ class TrackingProfileLoader(dict[str, TrackingProfile]):
 
         return profile
 
-    def _autoUnload(self, max_profiles: int = 5, ignore: str | None = None) -> None:
-        """Unload if too many profiles are loaded into memory at once."""
-        data = [(profile.is_modified,  # Sort modified profiles first
+    def _evict(self, keep_loaded: str) -> None:
+        """Unload if too many profiles are loaded into memory at once.
+        The argument to `keep_loaded` must be sanitised already.
+        """
+        sanitised = santise_profile_name(keep_loaded)
+        data = ((profile.is_modified,  # Sort modified profiles first
                  profile._last_accessed,  # Sort by recently accessed
                  name, profile)
-                for name, profile in self.items()]
-        data.sort(reverse=True)
+                for name, profile in self._profiles.items())
 
-        for i, (is_modified, load_time, name, profile) in enumerate(data):
-            if i < max_profiles or is_modified or name == ignore:
+        for i, (is_modified, load_time, name, profile) in enumerate(sorted(data, reverse=True)):
+            if i < self.max_profiles or is_modified or name == sanitised:
                 continue
-            del self[name]
+            del self._profiles[name]
 
 
-def get_filename(application: str) -> str:
-    """Get the filename for an application."""
-    sanitised = re.sub(r'[^a-zA-Z0-9]', '', application.lower())
-    return os.path.join(PROFILE_DIR, f'{sanitised}.{EXTENSION}')
+def santise_profile_name(profile_name: str) -> str:
+    """Get the sanitised version of a profile name."""
+    return re.sub(r'[^a-zA-Z0-9]', '', profile_name.lower())
+
+
+def get_filename(profile_name: str) -> str:
+    """Get the filename for a profile."""
+    return os.path.join(PROFILE_DIR, f'{santise_profile_name(profile_name)}.{EXTENSION}')
 
 
 def get_profile_names() -> list[str]:

mousetracks2/gui/main_window.py
@@ -26,7 +26,7 @@ from ..config.cli import CLI
 from ..config.settings import GlobalConfig
 from ..constants import COMPRESSION_FACTOR, COMPRESSION_THRESHOLD, DEFAULT_PROFILE_NAME, RADIAL_ARRAY_SIZE
 from ..constants import UPDATES_PER_SECOND, IS_EXE, TRACKING_DISABLE
-from ..file import PROFILE_DIR, get_profile_names, get_filename
+from ..file import PROFILE_DIR, get_profile_names, get_filename, santise_profile_name
 from ..legacy import colours
 from ..update import is_latest_version
 from ..utils import keycodes, get_cursor_pos
@@ -143,7 +143,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self._delete_keyboard_pressed = False
         self._delete_gamepad_pressed = False
         self._delete_network_pressed = False
-        self._profile_names = get_profile_names()
+        self._profile_names = {santise_profile_name(name): name for name in get_profile_names()}
         self._unsaved_profiles: set[str] = set()
         self._redrawing_profiles = False
         self._is_loading_profile = 0
@@ -750,15 +750,12 @@ class MainWindow(QtWidgets.QMainWindow):
         """
         self._current_profile = profile
 
-        # Update the profile order by removing the record and inserting it at the top
-        name_lower = profile.name.lower()
-        for i, name in enumerate(map(str.lower, self._profile_names)):
-            if name == name_lower:
-                del self._profile_names[i]
-                break
-
-        self._profile_names.insert(0, profile.name)
-        self.mark_profiles_unsaved(profile.name)
+        # Update the profile order
+        sanitised = santise_profile_name(profile.name)
+        if sanitised in self._profile_names:
+            del self._profile_names[sanitised]
+        self._profile_names = {sanitised: profile.name} | self._profile_names
+        self.mark_profiles_unsaved(sanitised)
         self._redraw_profile_combobox()
 
     def _redraw_profile_combobox(self) -> None:
@@ -773,11 +770,11 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Add the profiles
         self.ui.current_profile.clear()
-        for profile in self._profile_names:
-            if profile in self._unsaved_profiles:
-                self.ui.current_profile.addItem(f'*{profile}', profile)
+        for sanitised, profile_name in self._profile_names.items():
+            if sanitised in self._unsaved_profiles:
+                self.ui.current_profile.addItem(f'*{profile_name}', profile_name)
             else:
-                self.ui.current_profile.addItem(profile, profile)
+                self.ui.current_profile.addItem(profile_name, profile_name)
 
         # Change back to the previously selected profile
         if self.ui.auto_switch_profile.isChecked():
@@ -1573,10 +1570,11 @@ class MainWindow(QtWidgets.QMainWindow):
 
             # Load a legacy profile and switch to it
             case ipc.LoadLegacyProfile():
-                self._profile_names.append(message.name)
-                self._unsaved_profiles.add(message.name)
+                sanitised = santise_profile_name(message.name)
+                self._profile_names[sanitised] = message.name
+                self._unsaved_profiles.add(sanitised)
                 self._redraw_profile_combobox()
-                self.ui.current_profile.setCurrentIndex(self._profile_names.index(message.name))
+                self.ui.current_profile.setCurrentIndex(tuple(self._profile_names).index(sanitised))
 
             case ipc.ExportStatsSuccessful():
                 msg = AutoCloseMessageBox(self)
@@ -1869,7 +1867,10 @@ class MainWindow(QtWidgets.QMainWindow):
     @property
     def is_live(self) -> bool:
         """Determine if the visible data is live."""
-        return self.ui.current_profile.currentData() == self.current_profile.name
+        current_profile = self.ui.current_profile.currentData()
+        if current_profile is None:
+            return False
+        return santise_profile_name(current_profile) == santise_profile_name(self.current_profile.name)
 
     @property
     def mouse_tracking_enabled(self) -> bool:
@@ -2044,8 +2045,10 @@ class MainWindow(QtWidgets.QMainWindow):
         if msg.exec() == QtWidgets.QMessageBox.StandardButton.Yes:
             self.component.send_data(ipc.DeleteProfile(profile))
             self.mark_profiles_saved(profile)
-            del self._profile_names[self._profile_names.index(profile)]
-            self._unsaved_profiles.discard(profile)
+
+            sanitised = santise_profile_name(profile)
+            del self._profile_names[sanitised]
+            self._unsaved_profiles.discard(sanitised)
 
             self._redraw_profile_combobox()
             self.profile_changed(0)
@@ -2112,17 +2115,18 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def mark_profiles_saved(self, *profile_names: str) -> None:
         """Mark profiles as saved."""
-        self._unsaved_profiles -= set(profile_names)
+        self._unsaved_profiles -= set(map(santise_profile_name, profile_names))
         self.set_profile_modified_text()
 
     def mark_profiles_unsaved(self, *profile_names: str) -> None:
         """Mark profiles as unsaved."""
-        self._unsaved_profiles |= set(profile_names)
+        self._unsaved_profiles |= set(map(santise_profile_name, profile_names))
         self.set_profile_modified_text()
 
     def set_profile_modified_text(self) -> None:
         """Set the text if the profile has been modified."""
-        if self.ui.current_profile.currentData() in self._unsaved_profiles:
+        current_profile = self.ui.current_profile.currentData()
+        if current_profile is not None and santise_profile_name(current_profile) in self._unsaved_profiles:
             self.ui.profile_modified.setText('Yes')
             self.ui.profile_save.setEnabled(self.state != ipc.TrackingState.Stopped)
         else:

commit a08cff1bb0aa4b06696eec641ae01f5c7c3983c1

Fix bug with negative tick difference
The bug was introduced with 50a0cd8f0d80f36928451e5b3eed95420f7c4c82.

mousetracks2/components/processing.py
@@ -14,13 +14,13 @@ from ..config.cli import CLI
 from ..config.settings import GlobalConfig
 from ..exceptions import ExitRequest
 from ..export import Export
-from ..file import ArrayResolutionMap, MovementMaps, TrackingProfile, TrackingProfileLoader, get_filename
+from ..file import ArrayResolutionMap, MovementMaps, TrackingProfile, TrackingProfileLoader, get_filename, santise_profile_name
 from ..legacy import keyboard
 from ..utils import keycodes, get_cursor_pos
 from ..utils.math import calculate_line, calculate_distance, calculate_pixel_offset
 from ..utils.network import Interfaces
 from ..utils.system import monitor_locations
-from ..constants import DEFAULT_PROFILE_NAME, UPDATES_PER_SECOND, DOUBLE_CLICK_MS, DOUBLE_CLICK_TOL, RADIAL_ARRAY_SIZE
+from ..constants import DEFAULT_PROFILE_NAME, UPDATES_PER_SECOND, DOUBLE_CLICK_MS, DOUBLE_CLICK_TOL, RADIAL_ARRAY_SIZE, DEBUG
 from ..render import render, EmptyRenderError
 
 
@@ -374,15 +374,34 @@ class Processing(Component):
         # Convert back to array to send to GUI
         return np.asarray(image)
 
+    def _get_tick_diff(self, profile_name: str) -> int:
+        """Get the difference between elapsed ticks and recorded ticks.
+
+        This should always return a positive integer, but a check is
+        required as it's quite finicky and easy to break with updates.
+        """
+        profile = self.all_profiles[profile_name]
+        tick_diff = profile.elapsed - (profile.active + profile.inactive)
+        if tick_diff < 0:
+            raise RuntimeError(f'unexpected tick difference, should be a positive number, got {tick_diff} '
+                               f'(elapsed: {profile.elapsed}, active: {profile.active}, inactive: {profile.inactive})')
+        return tick_diff
+
     def _record_active_tick(self, profile_name: str, ticks: int) -> None:
         profile = self.all_profiles[profile_name]
         profile.active += ticks
         profile.daily_ticks[self.profile_age_days, 1] += ticks
 
+        if DEBUG:
+            self._get_tick_diff(profile_name)
+
     def _record_inactive_tick(self, profile_name: str, ticks: int) -> None:
         profile = self.all_profiles[profile_name]
         profile.inactive += ticks
         profile.daily_ticks[self.profile_age_days, 2] += ticks
+
+        if DEBUG:
+            self._get_tick_diff(profile_name)
 
     def _export_stats(self, message: ipc.ExportStats) -> None:
         """Export a stats CSV file."""
@@ -409,7 +428,6 @@ class Processing(Component):
 
         self.send_data(ipc.ExportStatsSuccessful(message))
 
-
     def _save(self, profile_name: str) -> bool:
         """Save a profile to disk.
         See `ipc.SaveReady` for information on why the `inactivity`
@@ -425,12 +443,7 @@ class Processing(Component):
         # temporarily add the current data to the profile
         # This is the same logic in the GUI
         inactivity_threshold = UPDATES_PER_SECOND * GlobalConfig.inactivity_time
-        tick_diff = profile.elapsed - (profile.active + profile.inactive)
-        # There's a bug where tick_diff is ending up as -1 and causing an error
-        # The cause is not known, so just check for it
-        if tick_diff < 0:
-            raise RuntimeError(f'unexpected tick difference, should be a positive number, got {tick_diff} '
-                               f'(elapsed: {profile.elapsed}, active: {profile.active}, inactive: {profile.inactive})')
+        tick_diff = self._get_tick_diff(profile_name)
         if tick_diff > inactivity_threshold:
             self._record_inactive_tick(profile_name, tick_diff)
         elif tick_diff:
@@ -631,7 +644,7 @@ class Processing(Component):
                     if message.profile_name in self.all_profiles:
                         profile_names.append(message.profile_name)
                 else:
-                    profile_names.extend(self.all_profiles)
+                    profile_names.extend(profile.name for profile in self.all_profiles.values())
 
                 for profile_name in profile_names:
                     profile = self.all_profiles[profile_name]

mousetracks2/file.py
@@ -693,16 +693,23 @@ class TrackingProfileLoader(MutableMapping):
     def __len__(self) -> int:
         return len(self._profiles)
 
+    def __contains__(self, key: Any) -> bool:
+        if not isinstance(key, str):
+            return False
+        sanitised = santise_profile_name(key)
+        return sanitised in self._profiles
+
     def _load_or_create_profile(self, profile_name: str) -> TrackingProfile:
         """Load in any missing data or create a new profile.
         This is in the place of `__missing__`, as the profile name gets
         sanitised before it reaches that point.
         """
         filename = get_filename(profile_name)
+        sanitised = santise_profile_name(profile_name)
         if os.path.exists(filename):
-            profile = self._profiles[profile_name] = TrackingProfile.load(filename)
+            profile = self._profiles[sanitised] = TrackingProfile.load(filename)
         else:
-            profile = self._profiles[profile_name] = TrackingProfile()
+            profile = self._profiles[sanitised] = TrackingProfile()
         self._evict(keep_loaded=santise_profile_name(profile_name))
 
         # Update the data