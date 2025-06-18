9a40c6b6816e2cd2595bbb8ed950cc609226c009
Peter Hunt <peter@huntfx.uk>
2025-06-18 23:53:18 +0100

    Create the first release
    This is the first official public release of MouseTracks!
    
    After originally starting work on the project in 2017, then picking it up again in early 2025, I'm excited to say it's finally ready to see the light of day.
    All the core features are in place, with a lot of effort put into making sure the application runs as smoothly and as reliably as possible.
    
    # Features
    - Live activity tracking
    - Per-application profiling
    - High quality rendering
    - Customisable artistic renders
    - Multi-monitor support
    - Interactive live GUI
    - Import/export data
    - Standalone application (no install required)
    - Option to launch on startup

--- mousetracks2/version.py
+++ mousetracks2/version.py
@@ -1,5 +1,20 @@
 """Set the current application version.
 This is used for both the application and build process.
+
+Updating this file will trigger a release using the version number.
+The process is as follows:
+1. Commit `version.py`.
+2. Create git tag named `v{VERSION}` (eg. v2.0.0).
+3. Create release, using the commit message as release notes.
+4. Build executable and attach to release.
+
+Some processing will be done on the commit message so the format can be
+kept simple. The recommend commit message format is as follows:
+    Created new option.
+    # Enhancements
+    - New option
+    # Fixes
+    - Fixed potential crash when thing happens ({commit_sha})
 """
 
 VERSION = '2.0.0'
